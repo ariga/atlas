b06999979dc1e6ac980956ceb4d3048984095cea
Rotem Tamir <rotemtamir@gmail.com>
2021-06-03 15:44:55 +0300

    apply go mashraki fmt

--- internal/integration/entinteg/ent/schema/default.go
+++ internal/integration/entinteg/ent/schema/default.go
@@ -11,10 +11,16 @@ type DefaultContainer struct {
 
 func (DefaultContainer) Fields() []ent.Field {
 	return []ent.Field{
-		field.String("stringdef").Default("default"),
-		field.Int("int").Default(1),
-		field.Bool("bool").Default(true),
-		field.Enum("enum").Values("1", "2").Default("1"),
-		field.Float("float").Default(1.5),
+		field.String("stringdef").
+			Default("default"),
+		field.Int("int").
+			Default(1),
+		field.Bool("bool").
+			Default(true),
+		field.Enum("enum").
+			Values("1", "2").
+			Default("1"),
+		field.Float("float").
+			Default(1.5),
 	}
 }
