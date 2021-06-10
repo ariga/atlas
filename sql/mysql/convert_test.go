6d364086da02ebd5d03ca25f4adc0db2ea346f44
Rotem Tamir <rotemtamir@gmail.com>
2021-06-10 14:03:09 +0300

    rename test

--- sql/mysql/convert_test.go
+++ sql/mysql/convert_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBuilder_Build(t *testing.T) {
+func TestConvertSchema(t *testing.T) {
 	spec := &schema.SchemaSpec{
 		Name: "schema",
 		Tables: []*schema.TableSpec{
