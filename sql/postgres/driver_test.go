6abf77725ec6eaf4a1821c4acf3040eeb3ab9002
Ariel Mashraki <ariel@mashraki.co.il>
2021-06-17 12:36:59 +0300

    sql/postgres: scan all standard postgres types

--- sql/postgres/driver_test.go
+++ sql/postgres/driver_test.go
@@ -46,25 +46,81 @@ func TestDriver_InspectTable(t *testing.T) {
 			},
 		},
 		{
-			name: "int types",
+			name: "column types",
 			before: func(m mock) {
 				m.version("130000")
 				m.tableExists("public", "users", true)
 				m.ExpectQuery(sqltest.Escape(columnsQuery)).
 					WithArgs("public", "users").
 					WillReturnRows(sqltest.Rows(`
- column_name | data_type | is_nullable | column_default | coalesce | coalesce | coalesce | character_set_name | collation_name | udt_name | is_identity | comment 
--------------+-----------+-------------+----------------+----------+----------+----------+--------------------+----------------+----------+-------------+---------
- id          | bigint    | NO          |                |        0 |       64 |        0 |                    |                | int8     | YES         | 
- rank        | integer   | YES         |                |        0 |       32 |        0 |                    |                | int4     | NO          | rank
+ column_name |      data_type      | is_nullable |         column_default          | character_maximum_length | numeric_precision | numeric_scale | character_set_name | collation_name | udt_name | is_identity | comment | typtype |  oid  
+-------------+---------------------+-------------+---------------------------------+--------------------------+-------------------+---------------+--------------------+----------------+----------+-------------+---------+---------+-------
+ id          | bigint              | NO          |                                 |                          |                64 |             0 |                    |                | int8     | NO          |         | b       |    20
+ rank        | integer             | YES         |                                 |                          |                32 |             0 |                    |                | int4     | NO          | rank    | b       |    23
+ c1          | smallint            | NO          |                                 |                          |                16 |             0 |                    |                | int2     | NO          |         | b       |    21
+ c2          | bit                 | NO          |                                 |                        1 |                   |               |                    |                | bit      | NO          |         | b       |  1560
+ c3          | bit varying         | NO          |                                 |                       10 |                   |               |                    |                | varbit   | NO          |         | b       |  1562
+ c4          | boolean             | NO          |                                 |                          |                   |               |                    |                | bool     | NO          |         | b       |    16
+ c5          | bytea               | NO          |                                 |                          |                   |               |                    |                | bytea    | NO          |         | b       |    17
+ c6          | character           | NO          |                                 |                      100 |                   |               |                    |                | bpchar   | NO          |         | b       |  1042
+ c7          | character varying   | NO          |                                 |                          |                   |               |                    |                | varchar  | NO          |         | b       |  1043
+ c8          | cidr                | NO          |                                 |                          |                   |               |                    |                | cidr     | NO          |         | b       |   650
+ c9          | circle              | NO          |                                 |                          |                   |               |                    |                | circle   | NO          |         | b       |   718
+ c10         | date                | NO          |                                 |                          |                   |               |                    |                | date     | NO          |         | b       |  1082
+ c11         | time with time zone | NO          |                                 |                          |                   |               |                    |                | timetz   | NO          |         | b       |  1266
+ c12         | double precision    | NO          |                                 |                          |                53 |               |                    |                | float8   | NO          |         | b       |   701
+ c13         | real                | NO          |                                 |                          |                24 |               |                    |                | float4   | NO          |         | b       |   700
+ c14         | json                | NO          |                                 |                          |                   |               |                    |                | json     | NO          |         | b       |   114
+ c15         | jsonb               | NO          |                                 |                          |                   |               |                    |                | jsonb    | NO          |         | b       |  3802
+ c16         | money               | NO          |                                 |                          |                   |               |                    |                | money    | NO          |         | b       |   790
+ c17         | numeric             | NO          |                                 |                          |                   |               |                    |                | numeric  | NO          |         | b       |  1700
+ c18         | numeric             | NO          |                                 |                          |                 4 |             4 |                    |                | numeric  | NO          |         | b       |  1700
+ c19         | integer             | NO          | nextval('t1_c19_seq'::regclass) |                          |                32 |             0 |                    |                | int4     | NO          |         | b       |    23
+ c20         | uuid                | NO          |                                 |                          |                   |               |                    |                | uuid     | NO          |         | b       |  2950
+ c21         | xml                 | NO          |                                 |                          |                   |               |                    |                | xml      | NO          |         | b       |   142
+ c22         | ARRAY               | YES         |                                 |                          |                   |               |                    |                | _int4    | NO          |         | b       |  1007
+ c23         | USER-DEFINED        | YES         |                                 |                          |                   |               |                    |                | ltree    | NO          |         | b       | 16535
+ c24         | USER-DEFINED        | NO          |                                 |                          |                   |               |                    |                | state    | NO          |         | e       | 16774
+`))
+				m.ExpectQuery(sqltest.Escape(`SELECT enumtypid, enumlabel FROM pg_enum WHERE enumtypid IN (?)`)).
+					WithArgs(16774).
+					WillReturnRows(sqltest.Rows(`
+ enumtypid | enumlabel
+-----------+-----------
+     16774 | on
+     16774 | off
 `))
 			},
 			expect: func(require *require.Assertions, t *schema.Table, err error) {
 				require.NoError(err)
 				require.Equal("users", t.Name)
 				require.EqualValues([]*schema.Column{
-					{Name: "id", Type: &schema.ColumnType{Raw: "bigint"}},
-					{Name: "rank", Type: &schema.ColumnType{Raw: "integer", Null: true}, Attrs: []schema.Attr{&schema.Comment{Text: "rank"}}},
+					{Name: "id", Type: &schema.ColumnType{Raw: "bigint", Type: &schema.IntegerType{T: "bigint", Size: 8}}},
+					{Name: "rank", Type: &schema.ColumnType{Raw: "integer", Null: true, Type: &schema.IntegerType{T: "integer", Size: 4}}, Attrs: []schema.Attr{&schema.Comment{Text: "rank"}}},
+					{Name: "c1", Type: &schema.ColumnType{Raw: "smallint", Type: &schema.IntegerType{T: "smallint", Size: 2}}},
+					{Name: "c2", Type: &schema.ColumnType{Raw: "bit", Type: &BitType{T: "bit", Len: 1}}},
+					{Name: "c3", Type: &schema.ColumnType{Raw: "bit varying", Type: &BitType{T: "bit varying", Len: 10}}},
+					{Name: "c4", Type: &schema.ColumnType{Raw: "boolean", Type: &schema.BoolType{T: "boolean"}}},
+					{Name: "c5", Type: &schema.ColumnType{Raw: "bytea", Type: &schema.BinaryType{T: "bytea"}}},
+					{Name: "c6", Type: &schema.ColumnType{Raw: "character", Type: &schema.StringType{T: "character", Size: 100}}},
+					{Name: "c7", Type: &schema.ColumnType{Raw: "character varying", Type: &schema.StringType{T: "character varying"}}},
+					{Name: "c8", Type: &schema.ColumnType{Raw: "cidr", Type: &NetworkType{T: "cidr"}}},
+					{Name: "c9", Type: &schema.ColumnType{Raw: "circle", Type: &schema.SpatialType{T: "circle"}}},
+					{Name: "c10", Type: &schema.ColumnType{Raw: "date", Type: &schema.TimeType{T: "date"}}},
+					{Name: "c11", Type: &schema.ColumnType{Raw: "time with time zone", Type: &schema.TimeType{T: "time with time zone"}}},
+					{Name: "c12", Type: &schema.ColumnType{Raw: "double precision", Type: &schema.FloatType{T: "double precision", Precision: 53}}},
+					{Name: "c13", Type: &schema.ColumnType{Raw: "real", Type: &schema.FloatType{T: "real", Precision: 24}}},
+					{Name: "c14", Type: &schema.ColumnType{Raw: "json", Type: &schema.JSONType{T: "json"}}},
+					{Name: "c15", Type: &schema.ColumnType{Raw: "jsonb", Type: &schema.JSONType{T: "jsonb"}}},
+					{Name: "c16", Type: &schema.ColumnType{Raw: "money", Type: &CurrencyType{T: "money"}}},
+					{Name: "c17", Type: &schema.ColumnType{Raw: "numeric", Type: &schema.DecimalType{T: "numeric"}}},
+					{Name: "c18", Type: &schema.ColumnType{Raw: "numeric", Type: &schema.DecimalType{T: "numeric", Precision: 4, Scale: 4}}},
+					{Name: "c19", Type: &schema.ColumnType{Raw: "integer", Type: &schema.IntegerType{T: "integer", Size: 4}}, Default: &schema.RawExpr{X: "nextval('t1_c19_seq'::regclass)"}},
+					{Name: "c20", Type: &schema.ColumnType{Raw: "uuid", Type: &UUIDType{T: "uuid"}}},
+					{Name: "c21", Type: &schema.ColumnType{Raw: "xml", Type: &XMLType{T: "xml"}}},
+					{Name: "c22", Type: &schema.ColumnType{Raw: "ARRAY", Null: true, Type: &ArrayType{T: "int4"}}},
+					{Name: "c23", Type: &schema.ColumnType{Raw: "USER-DEFINED", Null: true, Type: &UserDefinedType{T: "ltree"}}},
+					{Name: "c24", Type: &schema.ColumnType{Raw: "USER-DEFINED", Type: &EnumType{T: "state", ID: 16774, Values: []string{"on", "off"}}}},
 				}, t.Columns)
 			},
 		},
