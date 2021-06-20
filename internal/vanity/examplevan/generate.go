c4760407652a233a3e670710d7f8bc52db1c5dcd
Rotem Tamir <rotemtamir@gmail.com>
2021-06-20 13:23:43 +0300

    internal/vanity: generate resources for vanity url for go packages

---
+++ internal/vanity/examplevan/generate.go
@@ -0,0 +1,3 @@
+package examplevan
+
+//go:generate go run -mod=mod 4d63.com/vangen -out=../../../doc/website/static
---
+++ internal/vanity/go.mod
@@ -0,0 +1,5 @@
+module atlasgo.io/internal/vanity
+
+go 1.16
+
+require 4d63.com/vangen v1.1.3 // indirect
