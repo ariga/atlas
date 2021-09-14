f4bec03ef42e04138699f7e32d26fa7edfe14944
Rotem Tamir <rotemtamir@gmail.com>
2021-09-14 14:57:35 +0300

    sql/sqlspec: initial impl for sql Extension (#136)
    * sql/sqlspec: initial impl for sql Extension
    
    * removing sqlspec.File

---
+++ internal/integration/hclsqlspec/hclsqlspec_test.go
@@ -0,0 +1,65 @@
+package hclsqlspec
+
+import (
+	"testing"
+
+	"ariga.io/atlas/schema/schemaspec"
+	"ariga.io/atlas/schema/schemaspec/schemahcl"
+	"ariga.io/atlas/sql/sqlspec"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHCL_SQL(t *testing.T) {
+	file, err := decode(`
+schema "hi" {
+
+}
+
+table "users" {
+	schema = "hi"
+	
+	column "id" {
+		type = "uint"
+		null = false
+	}
+}
+`)
+	require.NoError(t, err)
+	require.EqualValues(t, &db{
+		Schemas: []*sqlspec.Schema{
+			{Name: "hi"},
+		},
+		Tables: []*sqlspec.Table{
+			{
+				Name:       "users",
+				SchemaName: "hi",
+				Columns: []*sqlspec.Column{
+					{
+						Name:     "id",
+						TypeName: "uint",
+						Null:     false,
+					},
+				},
+			},
+		},
+	}, file)
+}
+
+func decode(f string) (*db, error) {
+	res, err := schemahcl.Decode([]byte(f))
+	if err != nil {
+		return nil, err
+	}
+	s := db{}
+	if err := res.As(&s); err != nil {
+		return nil, err
+	}
+	return &s, nil
+}
+
+type db struct {
+	Schemas []*sqlspec.Schema `spec:"schema"`
+	Tables  []*sqlspec.Table  `spec:"table"`
+	schemaspec.Extension
+}
+
