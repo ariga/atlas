9c93c4de0fb8a9d36b2351bfa17c825461e41ccd
Rotem Tamir <rotemtamir@gmail.com>
2021-09-15 10:33:47 +0300

    internal/integration: basic HCL integration test

--- internal/integration/mysql_test.go
+++ internal/integration/mysql_test.go
@@ -49,23 +49,38 @@ func (s *mysqlSuite) SetupTest() {
 }
 
 func (s *mysqlSuite) TestHCL() {
+	s.applyHcl(`
+schema "test" {
+}
+
+table "users" {
+	schema = schema.test
+	column "email" {
+		type = "string"
+	}
+}
+`)
+	realm := s.loadRealm()
+	table, ok := realm.Schemas[0].Table("users")
+	s.True(ok, "expected users table")
+	column, ok := table.Column("email")
+	s.True(ok, "expected name column")
+	s.Equal("users", table.Name)
+	s.Equal("email", column.Name)
+	s.Equal(column.Type.Raw, "varchar(255)")
+	s.applyHcl(`
+schema "test" {
+}
+`)
+	s.TestEmptyRealm()
+}
+
+func (s *mysqlSuite) applyHcl(spec string) {
 	ctx := context.Background()
 	realm, err := s.drv.InspectRealm(ctx, &schema.InspectRealmOption{
 		Schemas: []string{"test"},
 	})
-
 	s.NoError(err)
-	spec := `
-schema "test" {
-}
-
-table "hello" {
-	schema = schema.test
-	column "world" {
-		type = "string"
-	}
-}
-`
 	file := schemaspec.File{}
 	err = schemahcl.Decode([]byte(spec), &file)
 	s.NoError(err)
