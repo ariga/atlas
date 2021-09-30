86163a155e3dd2ad383402b17be10eea2f077218
Yoni Davidson <yonidavidson@gmail.com>
2021-09-30 16:56:32 +0300

    sql/sqlspec: support foreign keys (#165)
    Support foreign keys

--- internal/integration/hclsqlspec/hclsqlspec_test.go
+++ internal/integration/hclsqlspec/hclsqlspec_test.go
@@ -33,6 +33,11 @@ table "users" {
 		default = true
 	}
 
+	column "account_active" {
+		type = "bool"
+		default = true
+	}
+
 	primary_key {
 		columns = table.users.column.id
 	}
@@ -44,6 +49,55 @@ table "users" {
 	index "active" {
 		unique = false
 		columns = table.users.column.active
+	}
+
+	foreign_key "fk" {
+		columns = table.users.column.account_active
+		ref_columns = table.accounts.column.active
+		on_delete = "SET NULL"
+	}
+}
+
+table "accounts" {
+	schema = "hi"
+	
+	column "id" {
+		type = "uint"
+		null = false
+		default = 123
+	}
+	column "age" {
+		type = "int"
+		null = false
+		default = 10
+	}
+	column "active" {
+		type = "bool"
+		default = true
+	}
+
+	column "user_active" {
+		type = "bool"
+		default = true
+	}
+
+	primary_key {
+		columns = table.accounts.column.id
+	}
+	
+	index "age" {
+		unique = true
+		columns = table.accounts.column.age
+	}
+	index "active" {
+		unique = false
+		columns = table.accounts.column.active
+	}
+
+	foreign_key "fk" {
+		columns = table.accounts.column.user_active
+		ref_columns = table.users.column.active
+		on_delete = "SET NULL"
 	}
 }
 
@@ -76,6 +130,12 @@ table "users" {
 						Null:     false,
 						Default:  &schemaspec.LiteralValue{V: "true"},
 					},
+					{
+						Name:     "account_active",
+						TypeName: "bool",
+						Null:     false,
+						Default:  &schemaspec.LiteralValue{V: "true"},
+					},
 				},
 				PrimaryKey: &sqlspec.PrimaryKey{
 					Columns: &schemaspec.Ref{
@@ -96,6 +156,81 @@ table "users" {
 						Columns: &schemaspec.Ref{
 							V: "$table.users.$column.active",
 						},
+					},
+				},
+				ForeignKeys: []*sqlspec.ForeignKey{
+					{
+						Symbol: "fk",
+						Columns: &schemaspec.Ref{
+							V: "$table.users.$column.account_active",
+						},
+						RefColumns: &schemaspec.Ref{
+							V: "$table.accounts.$column.active",
+						},
+						OnDelete: sqlspec.SetNull,
+					},
+				},
+			},
+			{
+				Name:       "accounts",
+				SchemaName: "hi",
+				Columns: []*sqlspec.Column{
+					{
+						Name:     "id",
+						TypeName: "uint",
+						Null:     false,
+						Default:  &schemaspec.LiteralValue{V: "123"},
+					},
+					{
+						Name:     "age",
+						TypeName: "int",
+						Null:     false,
+						Default:  &schemaspec.LiteralValue{V: "10"},
+					},
+					{
+						Name:     "active",
+						TypeName: "bool",
+						Null:     false,
+						Default:  &schemaspec.LiteralValue{V: "true"},
+					},
+					{
+						Name:     "user_active",
+						TypeName: "bool",
+						Null:     false,
+						Default:  &schemaspec.LiteralValue{V: "true"},
+					},
+				},
+				PrimaryKey: &sqlspec.PrimaryKey{
+					Columns: &schemaspec.Ref{
+						V: "$table.accounts.$column.id",
+					},
+				},
+				Indexes: []*sqlspec.Index{
+					{
+						Name:   "age",
+						Unique: true,
+						Columns: &schemaspec.Ref{
+							V: "$table.accounts.$column.age",
+						},
+					},
+					{
+						Name:   "active",
+						Unique: false,
+						Columns: &schemaspec.Ref{
+							V: "$table.accounts.$column.active",
+						},
+					},
+				},
+				ForeignKeys: []*sqlspec.ForeignKey{
+					{
+						Symbol: "fk",
+						Columns: &schemaspec.Ref{
+							V: "$table.accounts.$column.user_active",
+						},
+						RefColumns: &schemaspec.Ref{
+							V: "$table.users.$column.active",
+						},
+						OnDelete: sqlspec.SetNull,
 					},
 				},
 			},
--- sql/sqlspec/sqlspec.go
+++ sql/sqlspec/sqlspec.go
@@ -12,12 +12,12 @@ type (
 
 	// Table holds a specification for an SQL table.
 	Table struct {
-		Name       string      `spec:",name"`
-		SchemaName string      `spec:"schema"`
-		Columns    []*Column   `spec:"column"`
-		PrimaryKey *PrimaryKey `spec:"primary_key"`
-		//ForeignKeys []*ForeignKey
-		Indexes []*Index `spec:"index"`
+		Name        string        `spec:",name"`
+		SchemaName  string        `spec:"schema"`
+		Columns     []*Column     `spec:"column"`
+		PrimaryKey  *PrimaryKey   `spec:"primary_key"`
+		ForeignKeys []*ForeignKey `spec:"foreign_key"`
+		Indexes     []*Index      `spec:"index"`
 	}
 
 	// Column holds a specification for a column in an SQL table.
@@ -42,4 +42,27 @@ type (
 		Columns *schemaspec.Ref `spec:"columns"`
 		schemaspec.DefaultExtension
 	}
+
+	// ForeignKey holds a specification for the Foreign key of a table.
+	ForeignKey struct {
+		Symbol     string          `spec:",name"`
+		Columns    *schemaspec.Ref `spec:"columns"`
+		RefColumns *schemaspec.Ref `spec:"ref_columns"`
+		OnUpdate   ReferenceOption `spec:"on_update"`
+		OnDelete   ReferenceOption `spec:"on_delete"`
+		schemaspec.DefaultExtension
+	}
+
+	// ReferenceOption for constraint actions.
+	ReferenceOption string
+)
+
+// Reference options (actions) specified by ON UPDATE and ON DELETE
+// subclauses of the FOREIGN KEY clause.
+const (
+	NoAction   ReferenceOption = "NO ACTION"
+	Restrict   ReferenceOption = "RESTRICT"
+	Cascade    ReferenceOption = "CASCADE"
+	SetNull    ReferenceOption = "SET NULL"
+	SetDefault ReferenceOption = "SET DEFAULT"
 )
