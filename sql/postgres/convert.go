05f0501566ebdaa5e17ec1664572d384514ca217
Yoni Davidson <yonidavidson@gmail.com>
2021-11-03 08:49:33 +0200

    sql/convert: remove old api (#201)
    * remove sqlite
    
    * converting mysql
    
    * remove mysql convert
    
    * middle of work
    
    * code compiles
    
    * integration tests failing lightly
    
    * almost done
    
    * cleanup
    
    * mysql tests pass
    
    * working on ent
    
    * ent schema
    
    * burn with fire
    
    * remove n prefix mysql
    
    * clean postgres
    
    * remove sqlite
    
    * clean fmt

--- sql/postgres/convert.go
+++ sql/postgres/convert.go
@@ -10,10 +10,7 @@ import (
 	"strconv"
 	"strings"
 
-	"ariga.io/atlas/sql/internal/schemautil"
-	"ariga.io/atlas/sql/internal/sqlx"
 	"ariga.io/atlas/sql/schema"
-	"ariga.io/atlas/sql/schema/schemaspec"
 )
 
 // FormatType converts schema type to its column form in the database.
@@ -131,157 +128,9 @@ func (d *Driver) FormatType(t schema.Type) (string, error) {
 	return f, nil
 }
 
-// ConvertSchema converts a schemaspec.Schema and its associated tables into a schema.Schema.
-func (d *Driver) ConvertSchema(spec *schemaspec.Schema, tables []*schemaspec.Table) (*schema.Schema, error) {
-	return schemautil.ConvertSchema(spec, tables, d.ConvertTable)
-}
-
-// ConvertTable converts a schemaspec.Table to a schema.Table. Table conversion is done without converting
-// ForeignKeySpecs into ForeignKeys, as the target tables do not necessarily exist in the schema
-// at this point. Instead, the linking is done by the ConvertSchema function.
-func (d *Driver) ConvertTable(spec *schemaspec.Table, parent *schema.Schema) (*schema.Table, error) {
-	return schemautil.ConvertTable(spec, parent, d.ConvertColumn, d.ConvertPrimaryKey, d.ConvertIndex)
-}
-
-// ConvertPrimaryKey converts a schemaspec.PrimaryKey to a schema.Index.
-func (d *Driver) ConvertPrimaryKey(spec *schemaspec.PrimaryKey, parent *schema.Table) (*schema.Index, error) {
-	return schemautil.ConvertPrimaryKey(spec, parent)
-}
-
-// ConvertIndex converts an schemaspec.Index to a schema.Index.
-func (d *Driver) ConvertIndex(spec *schemaspec.Index, parent *schema.Table) (*schema.Index, error) {
-	return schemautil.ConvertIndex(spec, parent)
-}
-
-// ConvertColumn converts a schemaspec.Column into a schema.Column.
-func (d *Driver) ConvertColumn(spec *schemaspec.Column, _ *schema.Table) (*schema.Column, error) {
-	const driver = "postgres"
-	if override := spec.Override(sqlx.VersionPermutations(driver, d.version)...); override != nil {
-		if err := schemautil.Override(spec, override); err != nil {
-			return nil, err
-		}
-	}
-	return schemautil.ConvertColumn(spec, d.ConvertColumnType)
-}
-
-// ConvertColumnType converts a schemaspec.Column into a concrete MySQL schema.Type.
-func (d *Driver) ConvertColumnType(spec *schemaspec.Column) (schema.Type, error) {
-	switch schemaspec.Type(spec.Type) {
-	case schemaspec.TypeInt, schemaspec.TypeInt8, schemaspec.TypeInt16,
-		schemaspec.TypeInt64, schemaspec.TypeUint, schemaspec.TypeUint8,
-		schemaspec.TypeUint16, schemaspec.TypeUint64:
-		return convertInteger(spec)
-	case schemaspec.TypeString:
-		return convertString(spec)
-	case schemaspec.TypeEnum:
-		return convertEnum(spec)
-	case schemaspec.TypeDecimal:
-		return convertDecimal(spec)
-	case schemaspec.TypeFloat:
-		return convertFloat(spec)
-	case schemaspec.TypeTime:
-		return &schema.TimeType{T: tTimestamp}, nil
-	case schemaspec.TypeBinary:
-		return &schema.BinaryType{T: tBytea}, nil
-	case schemaspec.TypeBoolean:
-		return &schema.BoolType{T: tBoolean}, nil
-	}
-	return parseRawType(spec)
-}
-
-func convertInteger(spec *schemaspec.Column) (schema.Type, error) {
-	if strings.HasPrefix(spec.Type, "u") {
-		return nil, fmt.Errorf("postgres: unsigned integers currently not supported")
-	}
-	typ := &schema.IntegerType{}
-	switch schemaspec.Type(spec.Type) {
-	case schemaspec.TypeInt8:
-		return nil, fmt.Errorf("postgres: 8-bit integers not supported")
-	case schemaspec.TypeInt16:
-		typ.T = tSmallInt
-	case schemaspec.TypeInt:
-		typ.T = tInteger
-	case schemaspec.TypeInt64:
-		typ.T = tBigInt
-	default:
-		return nil, fmt.Errorf("mysql: unknown integer column type %q", spec.Type)
-	}
-	return typ, nil
-}
-
 // maxCharSize defines the maximum size of limited character types in Postgres (10 MB).
 // https://github.com/postgres/postgres/blob/REL_13_STABLE/src/include/access/htup_details.h#L585
 const maxCharSize = 10 << 20
-
-func convertString(spec *schemaspec.Column) (schema.Type, error) {
-	st := &schema.StringType{
-		Size: 255,
-	}
-	if attr, ok := spec.Attr("size"); ok {
-		s, err := attr.Int()
-		if err != nil {
-			return nil, err
-		}
-		st.Size = s
-	}
-	switch {
-	case st.Size < maxCharSize:
-		st.T = tVarChar
-	default:
-		st.T = tText
-	}
-	return st, nil
-}
-
-func convertEnum(spec *schemaspec.Column) (schema.Type, error) {
-	attr, ok := spec.Attr("values")
-	if !ok {
-		return nil, errors.New("postgres: expected enum fields to have values")
-	}
-	list, err := attr.Strings()
-	if err != nil {
-		return nil, err
-	}
-	return &schema.EnumType{Values: list}, nil
-}
-
-func convertDecimal(spec *schemaspec.Column) (schema.Type, error) {
-	dt := &schema.DecimalType{
-		T: tDecimal,
-	}
-	if precision, ok := spec.Attr("precision"); ok {
-		p, err := precision.Int()
-		if err != nil {
-			return nil, err
-		}
-		dt.Precision = p
-	}
-	if scale, ok := spec.Attr("scale"); ok {
-		s, err := scale.Int()
-		if err != nil {
-			return nil, err
-		}
-		dt.Scale = s
-	}
-	return dt, nil
-}
-
-func convertFloat(spec *schemaspec.Column) (schema.Type, error) {
-	ft := &schema.FloatType{
-		T: tReal,
-	}
-	if precision, ok := spec.Attr("precision"); ok {
-		p, err := precision.Int()
-		if err != nil {
-			return nil, err
-		}
-		ft.Precision = p
-	}
-	if ft.Precision > 23 {
-		ft.T = tDouble
-	}
-	return ft, nil
-}
 
 // columnDesc represents a column descriptor.
 type columnDesc struct {
@@ -293,14 +142,6 @@ type columnDesc struct {
 	typtype   string
 	typid     int64
 	parts     []string
-}
-
-func parseRawType(spec *schemaspec.Column) (schema.Type, error) {
-	cm, err := parseColumn(spec.Type)
-	if err != nil {
-		return nil, err
-	}
-	return columnType(cm), nil
 }
 
 func parseColumn(s string) (*columnDesc, error) {
