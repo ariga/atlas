449eccc5c666914f83bb5181cb5044ed4faab4cf
Yoni Davidson <yoni@ariga.io>
2021-11-03 11:52:28 +0200

    almost done naming

--- cmd/atlas/internal/driver/commander.go
+++ cmd/atlas/internal/driver/commander.go
@@ -1,4 +1,4 @@
-package commander
+package driver
 
 import (
 	"context"
---
+++ cmd/atlas/internal/driver/schema/schema.go
@@ -0,0 +1,15 @@
+package schema
+
+import (
+	"context"
+
+	"ariga.io/atlas/cmd/atlas/internal/driver"
+)
+
+func Inspect(ctx context.Context, d driver.Driver) ([]byte, error) {
+	return nil, nil
+}
+
+func Apply(ctx context.Context, d driver.Driver) error {
+	return nil
+}
