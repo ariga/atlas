aa4ce98ddf453f11309f77f4f3f0800907b6b6fa
Yoni Davidson <yoni@ariga.io>
2021-11-04 15:10:35 +0200

    add apply

---
+++ cmd/atlas/cmd/apply.go
@@ -0,0 +1,27 @@
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// applyCmd represents the apply command
+var applyCmd = &cobra.Command{
+	Use:   "apply",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("apply called")
+	},
+}
+
+func init() {
+	schemaCmd.AddCommand(applyCmd)
+}
