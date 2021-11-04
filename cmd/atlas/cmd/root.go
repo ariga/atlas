b61ad61c99f0dadb647ca361b8020580ad7a472c
Yoni Davidson <yoni@ariga.io>
2021-11-04 13:46:27 +0200

    start of the cli

--- cmd/atlas/cmd/root.go
+++ cmd/atlas/cmd/root.go
@@ -4,21 +4,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var cfgFile string
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "atlas",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Work with any data source  from the command line.\n",
+	Long:  `Work with any data source  from the command line.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
