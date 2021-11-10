baa2332552580405d114690a51be4c92f80b3603
Yoni Davidson <yoni@ariga.io>
2021-11-10 17:01:03 +0200

    add printing as migration file

--- cmd/atlas/apply.go
+++ cmd/atlas/apply.go
@@ -58,13 +58,13 @@ func applyRun(d *Driver, u schemaUnmarshaler, dsn string, file string) {
 	cobra.CheckErr(err)
 	changes, err := d.SchemaDiff(s, &desired)
 	cobra.CheckErr(err)
-	schemaCmd.Println("Planned changes:")
+	schemaCmd.Println("-- Planned changes:")
 	for _, ch := range changes {
 		desc, err := changeDescriptor(ctx, ch, d)
 		cobra.CheckErr(err)
-		schemaCmd.Println(desc.typ, ":", desc.subject)
+		schemaCmd.Println("--",desc.typ, ":", desc.subject)
 		for _, q := range desc.queries {
-			schemaCmd.Println("*", q)
+			schemaCmd.Println( q,";")
 		}
 	}
 	prompt := promptui.Select{
