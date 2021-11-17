3fd16133bd4929bbe7ccb877ca3a5207a5360e84
Yoni Davidson <yoni@ariga.io>
2021-11-17 15:15:38 +0200

    add tag version

--- scripts/build.bash
+++ scripts/build.bash
@@ -5,13 +5,15 @@ package_split=(${package//\// })
 package_name=${package_split[3]}
 
 platforms=("windows/amd64" "darwin/amd64")
+TAG=$(git describe --tags --abbrev=0)
+
 
 for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name=$package_name'-'$GOOS'-'$GOARCH
+    output_name=$package_name'-'$GOOS'-'$GOARCH'-'$TAG
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
