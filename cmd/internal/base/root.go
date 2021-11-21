d8a229bca225878f0983fd17569836f1a0590ef4
Yoni Davidson <yonidavidson@gmail.com>
2021-11-21 11:00:16 +0200

    update main message (#241)

--- cmd/internal/base/root.go
+++ cmd/internal/base/root.go
@@ -5,5 +5,5 @@ import "github.com/spf13/cobra"
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "atlas",
-	Short: "Work with any data source from the command line.",
+	Short: "A database toolkit.",
 }
