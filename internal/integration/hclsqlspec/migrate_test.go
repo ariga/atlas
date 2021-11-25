d2ef939da56a32a8764b86854d10c345570bf096
Rotem Tamir <rotemtamir@gmail.com>
2021-11-25 11:18:07 +0200

    sql/migratespec: support modify_table and add_column (#246)

---
+++ internal/integration/hclsqlspec/migrate_test.go
@@ -0,0 +1,39 @@
+package hclsqlspec
+
+import (
+	"testing"
+
+	"ariga.io/atlas/schema/schemaspec/schemahcl"
+	"ariga.io/atlas/sql/sqlspec"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMigrate(t *testing.T) {
+	f := `
+modify_table {
+	table = "users"
+	add_column {
+		column "id" {
+			type = "int"
+		}
+	}
+}
+`
+	var test struct {
+		Changes []sqlspec.Change `spec:""`
+	}
+	err := schemahcl.Unmarshal([]byte(f), &test)
+	require.NoError(t, err)
+	require.EqualValues(t, &sqlspec.ModifyTable{
+		Table: "users",
+		Changes: []sqlspec.Change{
+			&sqlspec.AddColumn{
+				Column: &sqlspec.Column{
+					Name:     "id",
+					Null:     false,
+					TypeName: "int",
+				},
+			},
+		},
+	}, test.Changes[0])
+}
