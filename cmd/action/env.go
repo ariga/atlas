97f1f1af9083fa89e3cd9ae222b0f85a4de6d4e6
Yoni Davidson <yoni@ariga.io>
2022-01-04 16:30:20 +0200

    cmd/env: extend long help

--- cmd/action/env.go
+++ cmd/action/env.go
@@ -11,8 +11,12 @@ import (
 var envCmd = &cobra.Command{
 	Use:   "env",
 	Short: "Print atlas env params",
-	Long: `"ATLAS_NO_UPDATE_NOTIFIER": On any command, the CLI will check for updates with the GitHub public API once
-	every 24 hours. To cancel this behavior, set the environment parameter "ATLAS_NO_UPDATE_NOTIFIER".`,
+	Long: `Env prints atlas environment information.
+Every set environment param will print in the form of NAME=VALUE.
+
+List of supported environment parameters:
+"ATLAS_NO_UPDATE_NOTIFIER": On any command, the CLI will check for updates with the GitHub public API once every 24 hours.
+To cancel this behavior, set the environment parameter "ATLAS_NO_UPDATE_NOTIFIER".`,
 	Run: func(cmd *cobra.Command, args []string) {
 		keys := []string{update.AtlasNoUpdateNotifier}
 		for _, k := range keys {
