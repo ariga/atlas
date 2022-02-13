e7c50f697d428a337273313b7b27f189253af1ed
Yoni Davidson <yoni@ariga.io>
2022-02-13 11:33:32 +0200

    start integration test

--- internal/integration/mysql_test.go
+++ internal/integration/mysql_test.go
@@ -36,8 +36,12 @@ var myTests struct {
 func myRun(t *testing.T, fn func(*myTest)) {
 	myTests.Do(func() {
 		myTests.drivers = make(map[string]*myTest)
-		for version, port := range map[string]int{"56": 3306, "57": 3307, "8": 3308, "Maria107": 4306, "Maria102": 4307, "Maria103": 4308} {
-			db, err := sql.Open("mysql", fmt.Sprintf("root:pass@tcp(localhost:%d)/test?parseTime=True", port))
+		for version, port := range map[string]int{"56": 3306, "57": 3307, "8": 3308, "Maria107": 4306, "Maria102": 4307, "Maria103": 4308, "tiDB": 4309} {
+			password := ":pass"
+			if version == "tiDB" {
+				password = ""
+			}
+			db, err := sql.Open("mysql", fmt.Sprintf("root%s@tcp(localhost:%d)/test?parseTime=True", password, port))
 			require.NoError(t, err)
 			drv, err := mysql.Open(db)
 			require.NoError(t, err)
