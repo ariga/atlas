4aa7ea556716d86bf86e518f8839b8b24bd1b2a1
Zeev Manilovich <zeevmanilovich@gmail.com>
2022-03-02 16:09:42 +0200

    global var and fix tests

--- cmd/action/env.go
+++ cmd/action/env.go
@@ -13,30 +13,28 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	RootCmd.AddCommand(NewEnvCmd())
-}
-
-// NewEnvCmd returns a new *cobra.Command that runs the env command.
-func NewEnvCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:   "env",
-		Short: "Print atlas environment variables.",
-		Long: "`atlas env` " + `prints atlas environment information.
+// EnvCmd is the "atlas env" command.
+var EnvCmd = &cobra.Command{
+	Use:   "env",
+	Short: "Print atlas environment variables.",
+	Long: `'atlas env' prints atlas environment information.
 
 Every set environment param will be printed in the form of NAME=VALUE.
 
 List of supported environment parameters:
-- *ATLAS_NO_UPDATE_NOTIFIER*: On any command, the CLI will check for new releases using the GitHub API.
+* ATLAS_NO_UPDATE_NOTIFIER: On any command, the CLI will check for new releases using the GitHub API.
   This check will happen at most once every 24 hours. To cancel this behavior, set the environment 
   variable "ATLAS_NO_UPDATE_NOTIFIER".`,
-		Run: func(cmd *cobra.Command, args []string) {
-			keys := []string{update.AtlasNoUpdateNotifier}
-			for _, k := range keys {
-				if v, ok := os.LookupEnv(k); ok {
-					cmd.Println(fmt.Sprintf("%s=%s", k, v))
-				}
+	Run: func(cmd *cobra.Command, args []string) {
+		keys := []string{update.AtlasNoUpdateNotifier}
+		for _, k := range keys {
+			if v, ok := os.LookupEnv(k); ok {
+				cmd.Println(fmt.Sprintf("%s=%s", k, v))
 			}
-		},
-	}
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(EnvCmd)
 }
