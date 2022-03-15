0d03007c5a02d815eaa7991605e33e2e544f5b1a
Jannik C <github@masseelch.de>
2022-03-15 13:31:54 +0100

    cmd/action: move internal/docker to cmd/action and work with os.Exec instead

--- cmd/action/docker_test.go
+++ cmd/action/docker_test.go
@@ -1,35 +1,41 @@
-package docker
+// Copyright 2021-present The Atlas Authors. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+package action_test
 
 import (
 	"context"
 	"database/sql"
+	"io/ioutil"
 	"os/exec"
 	"testing"
 	"time"
 
+	"ariga.io/atlas/cmd/action"
 	"github.com/stretchr/testify/require"
 )
 
 func TestClient_Up(t *testing.T) {
-	c, err := NewClient()
-	require.NoError(t, err)
 	ctx := context.Background()
 
 	// invalid config
-	_, err = c.Up(ctx, &Config{})
+	_, err := (&action.DockerConfig{}).Run(ctx)
 	require.Error(t, err)
 
 	// MySQL
-	cfg, err := MySQL("latest")
+	cfg, err := action.MySQL("latest", action.Out(ioutil.Discard))
 	require.NoError(t, err)
-	ct, err := c.Up(context.Background(), cfg)
+	ct, err := cfg.Run(ctx)
 	require.NoError(t, err)
 	defer func(t *testing.T, ctx context.Context, id string) {
 		require.NoError(t, ct.Down(ctx))
 		require.Error(t, exec.Command("docker", "inspect", ct.ID).Run()) //nolint:gosec
 	}(t, ctx, ct.ID)
 	require.NoError(t, ct.Wait(ctx, time.Minute))
-	db, err := sql.Open(ct.Driver(), ct.DSN())
+	dsn, err := ct.DSN()
+	require.NoError(t, err)
+	db, err := sql.Open(ct.Driver(), dsn)
 	require.NoError(t, err)
 	require.NoError(t, db.Ping())
 	require.NoError(t, exec.Command("docker", "inspect", ct.ID).Run()) //nolint:gosec
