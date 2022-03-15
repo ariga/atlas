3f3be633179ea564eb7a3fce7188e46ab433f634
Jannik C <github@masseelch.de>
2022-03-15 09:57:05 +0100

    internal/docker: disable gosec in tests

--- internal/docker/client_test.go
+++ internal/docker/client_test.go
@@ -24,13 +24,13 @@ func TestClient_Up(t *testing.T) {
 	require.NoError(t, err)
 	ct, err := c.Up(context.Background(), cfg)
 	require.NoError(t, err)
-	defer func() {
+	defer func(t *testing.T, ctx context.Context, id string) {
 		require.NoError(t, ct.Down(ctx))
-		require.Error(t, exec.Command("docker", "inspect", ct.ID).Run())
-	}()
+		require.Error(t, exec.Command("docker", "inspect", ct.ID).Run()) //nolint:gosec
+	}(t, ctx, ct.ID)
 	require.NoError(t, ct.Wait(ctx, time.Minute))
 	db, err := sql.Open(ct.Driver(), ct.DSN())
 	require.NoError(t, err)
 	require.NoError(t, db.Ping())
-	require.NoError(t, exec.Command("docker", "inspect", ct.ID).Run())
+	require.NoError(t, exec.Command("docker", "inspect", ct.ID).Run()) //nolint:gosec
 }
