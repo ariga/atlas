e6cc4d161d392c218f67b1b66d9b2b06f07245d0
Ariel Mashraki <7413593+a8m@users.noreply.github.com>
2022-04-29 23:18:46 +0300

    Update cmd/atlascmd/atlascmd.go

--- cmd/atlascmd/atlascmd.go
+++ cmd/atlascmd/atlascmd.go
@@ -2,7 +2,7 @@
 // This source code is licensed under the Apache 2.0 license found
 // in the LICENSE file in the root directory of this source tree.
 
-// Package atlascmd holds the entire Root commands used to build
+// Package atlascmd holds the atlas commands used to build
 // an atlas distribution.
 package atlascmd
 
