694777e39ab175ac2c22719257eded36e4ae313d
Rotem Tamir <rotemtamir@gmail.com>
2022-05-12 09:19:17 +0300

    rename env

--- cmd/atlascmd/project.go
+++ cmd/atlascmd/project.go
@@ -16,11 +16,11 @@ const projectFileName = "atlas.hcl"
 
 // projectFile represents an atlas.hcl file.
 type projectFile struct {
-	Envs []*Environment `spec:"env"`
+	Envs []*Env `spec:"env"`
 }
 
-// Environment represents an Atlas environment.
-type Environment struct {
+// Env represents an Atlas environment.
+type Env struct {
 	// Name for this environment.
 	Name string `spec:"name,name"`
 
@@ -41,7 +41,7 @@ type Environment struct {
 
 // LoadEnv reads the project file in path, and loads the environment
 // with the provided name into env.
-func LoadEnv(path string, name string) (*Environment, error) {
+func LoadEnv(path string, name string) (*Env, error) {
 	b, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
@@ -50,7 +50,7 @@ func LoadEnv(path string, name string) (*Environment, error) {
 	if err := schemahcl.New().Eval(b, &project, nil); err != nil {
 		return nil, fmt.Errorf("error reading project file: %w", err)
 	}
-	projEnvs := make(map[string]*Environment)
+	projEnvs := make(map[string]*Env)
 	for _, e := range project.Envs {
 		if _, ok := projEnvs[e.Name]; ok {
 			return nil, fmt.Errorf("duplicate environment name %q", e.Name)
@@ -71,8 +71,8 @@ func LoadEnv(path string, name string) (*Environment, error) {
 }
 
 // activeEnv represents the environment selected by the user if such one exists.
-var activeEnv *Environment
+var activeEnv *Env
 
 func init() {
-	schemaspec.Register("env", &Environment{})
+	schemaspec.Register("env", &Env{})
 }
--- cmd/atlascmd/project_test.go
+++ cmd/atlascmd/project_test.go
@@ -26,10 +26,10 @@ env "local" {
 	require.NoError(t, err)
 	path := filepath.Join(d, projectFileName)
 	t.Run("ok", func(t *testing.T) {
-		env := &Environment{}
+		env := &Env{}
 		env, err = LoadEnv(path, "local")
 		require.NoError(t, err)
-		require.EqualValues(t, &Environment{
+		require.EqualValues(t, &Env{
 			Name:    "local",
 			URL:     "mysql://root:pass@localhost:3306/",
 			DevURL:  "docker://mysql/8",
