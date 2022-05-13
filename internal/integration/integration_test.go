e4bc78fe32a288a3876164c0920c395ffeb97f10
Jannik Clausen <github@masseelch.de>
2022-05-13 11:01:56 +0200

    cmd/atlascmd: move and fix test for version command and build

--- internal/integration/integration_test.go
+++ internal/integration/integration_test.go
@@ -406,53 +406,6 @@ func testCLISchemaDiff(t T, dsn string) {
 	require.Contains(t, stdout.String(), "Schemas are synced, no changes to be made.")
 }
 
-func TestCLI_Version(t *testing.T) {
-
-	require.NoError(t, exec.Command("go", "run", "-mod=mod", "ariga.io/atlas/cmd/atlas").Run())
-	tests := []struct {
-		name     string
-		cmd      *exec.Cmd
-		expected string
-	}{
-		{
-			name: "dev mode",
-			cmd: exec.Command("go", "run", "ariga.io/atlas/cmd/atlas",
-				"version",
-			),
-			expected: "atlas CLI version - development\nhttps://github.com/ariga/atlas/releases/latest\n",
-		},
-		{
-			name: "release",
-			cmd: exec.Command("go", "run",
-				"-ldflags",
-				"-X ariga.io/atlas/cmd/action.version=v1.2.3",
-				"ariga.io/atlas/cmd/atlas",
-				"version",
-			),
-			expected: "atlas CLI version v1.2.3\nhttps://github.com/ariga/atlas/releases/tag/v1.2.3\n",
-		},
-		{
-			name: "canary",
-			cmd: exec.Command("go", "run",
-				"-ldflags",
-				"-X ariga.io/atlas/cmd/action.version=v0.3.0-6539f2704b5d-canary",
-				"ariga.io/atlas/cmd/atlas",
-				"version",
-			),
-			expected: "atlas CLI version v0.3.0-6539f2704b5d-canary\nhttps://github.com/ariga/atlas/releases/latest\n",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			t.Setenv("ATLAS_NO_UPDATE_NOTIFIER", "true")
-			stdout := bytes.NewBuffer(nil)
-			tt.cmd.Stdout = stdout
-			require.NoError(t, tt.cmd.Run())
-			require.Equal(t, tt.expected, stdout.String())
-		})
-	}
-}
-
 func ensureNoChange(t T, tables ...*schema.Table) {
 	realm := t.loadRealm()
 	require.Equal(t, len(realm.Schemas[0].Tables), len(tables))
