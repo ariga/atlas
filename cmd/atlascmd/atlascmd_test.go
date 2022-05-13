f451633409d98a3432e43d7a60f9871170d042ce
Jannik Clausen <12862103+masseelch@users.noreply.github.com>
2022-05-13 11:10:57 +0200

    cmd/atlascmd: move and fix test for version command and build (#792)

--- cmd/atlascmd/atlascmd_test.go
+++ cmd/atlascmd/atlascmd_test.go
@@ -5,7 +5,9 @@
 package atlascmd
 
 import (
+	"bytes"
 	"os"
+	"os/exec"
 	"testing"
 
 	"ariga.io/atlas/cmd/atlascmd/update"
@@ -25,4 +27,51 @@ func TestEnv_Set(t *testing.T) {
 	out, err := runCmd(Root, "env")
 	require.NoError(t, err)
 	require.Equal(t, "ATLAS_NO_UPDATE_NOTIFIER=test\n", out)
+}
+
+func TestCLI_Version(t *testing.T) {
+	// Required to have a clean "stderr" while running first time.
+	require.NoError(t, exec.Command("go", "run", "-mod=mod", "ariga.io/atlas/cmd/atlas").Run())
+	tests := []struct {
+		name     string
+		cmd      *exec.Cmd
+		expected string
+	}{
+		{
+			name: "dev mode",
+			cmd: exec.Command("go", "run", "ariga.io/atlas/cmd/atlas",
+				"version",
+			),
+			expected: "atlas version - development\nhttps://github.com/ariga/atlas/releases/latest\n",
+		},
+		{
+			name: "release",
+			cmd: exec.Command("go", "run",
+				"-ldflags",
+				"-X ariga.io/atlas/cmd/atlascmd.version=v1.2.3",
+				"ariga.io/atlas/cmd/atlas",
+				"version",
+			),
+			expected: "atlas version v1.2.3\nhttps://github.com/ariga/atlas/releases/tag/v1.2.3\n",
+		},
+		{
+			name: "canary",
+			cmd: exec.Command("go", "run",
+				"-ldflags",
+				"-X ariga.io/atlas/cmd/atlascmd.version=v0.3.0-6539f2704b5d-canary",
+				"ariga.io/atlas/cmd/atlas",
+				"version",
+			),
+			expected: "atlas version v0.3.0-6539f2704b5d-canary\nhttps://github.com/ariga/atlas/releases/latest\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv("ATLAS_NO_UPDATE_NOTIFIER", "true")
+			stdout := bytes.NewBuffer(nil)
+			tt.cmd.Stdout = stdout
+			require.NoError(t, tt.cmd.Run())
+			require.Equal(t, tt.expected, stdout.String())
+		})
+	}
 }
