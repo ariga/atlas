925d2f8e82d2044454d9c840b091595ef9f5c089
Ariel Mashraki <ariel@mashraki.co.il>
2022-06-23 10:46:24 +0300

    sql/sqlcheck: skip temporary resources in destructive analyzer

---
+++ sql/sqlcheck/analyzers_test.go
@@ -0,0 +1,224 @@
+// Copyright 2021-present The Atlas Authors. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+package sqlcheck_test
+
+import (
+	"context"
+	"testing"
+
+	"ariga.io/atlas/sql/migrate"
+	"ariga.io/atlas/sql/schema"
+	"ariga.io/atlas/sql/sqlcheck"
+	"ariga.io/atlas/sql/sqlclient"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDestructive_DropTable(t *testing.T) {
+	var (
+		report *sqlcheck.Report
+		pass   = &sqlcheck.Pass{
+			Dev: &sqlclient.Client{},
+			File: &sqlcheck.File{
+				File: testFile{name: "1.sql"},
+				Changes: []*sqlcheck.Change{
+					{
+						Stmt: "DROP TABLE `users`",
+						Changes: schema.Changes{
+							&schema.DropTable{
+								T: schema.NewTable("users").
+									SetSchema(schema.New("test")),
+							},
+						},
+					},
+					{
+						Stmt: "DROP TABLE `posts`",
+						Changes: schema.Changes{
+							&schema.DropTable{
+								T: schema.NewTable("posts").
+									SetSchema(schema.New("test")),
+							},
+						},
+					},
+					{
+						Stmt: "CREATE TABLE `posts`",
+						Changes: schema.Changes{
+							&schema.AddTable{
+								T: schema.NewTable("posts").
+									SetSchema(schema.New("test")),
+							},
+						},
+					},
+				},
+			},
+			Reporter: sqlcheck.ReportWriterFunc(func(r sqlcheck.Report) {
+				report = &r
+			}),
+		}
+	)
+	err := sqlcheck.Destructive.Analyze(context.Background(), pass)
+	require.NoError(t, err)
+	require.Equal(t, `Destructive changes detected in file 1.sql`, report.Text)
+	require.Len(t, report.Diagnostics, 2)
+	require.Equal(t, `Dropping table "users"`, report.Diagnostics[0].Text)
+	require.Equal(t, `Dropping table "posts"`, report.Diagnostics[1].Text)
+}
+
+func TestDestructive_SkipTemporaryTable(t *testing.T) {
+	var (
+		report *sqlcheck.Report
+		pass   = &sqlcheck.Pass{
+			Dev: &sqlclient.Client{},
+			File: &sqlcheck.File{
+				File: testFile{name: "1.sql"},
+				Changes: []*sqlcheck.Change{
+					{
+						Stmt: "DROP TABLE `users`",
+						Changes: schema.Changes{
+							&schema.AddTable{
+								T: schema.NewTable("users").
+									SetSchema(schema.New("test")),
+							},
+							&schema.DropTable{
+								T: schema.NewTable("users").
+									SetSchema(schema.New("test")),
+							},
+						},
+					},
+				},
+			},
+			Reporter: sqlcheck.ReportWriterFunc(func(r sqlcheck.Report) {
+				report = &r
+			}),
+		}
+	)
+	err := sqlcheck.Destructive.Analyze(context.Background(), pass)
+	require.NoError(t, err)
+	require.Nil(t, report, "no report")
+}
+
+func TestDestructive_DropSchema(t *testing.T) {
+	var (
+		report *sqlcheck.Report
+		pass   = &sqlcheck.Pass{
+			Dev: &sqlclient.Client{},
+			File: &sqlcheck.File{
+				File: testFile{name: "1.sql"},
+				Changes: []*sqlcheck.Change{
+					{
+						Stmt: "DROP SCHEMA `test`",
+						Changes: schema.Changes{
+							&schema.DropSchema{
+								S: schema.New("test").
+									AddTables(
+										schema.NewTable("users"),
+										schema.NewTable("orders"),
+									),
+							},
+						},
+					},
+					{
+						Stmt: "DROP SCHEMA `market`",
+						Changes: schema.Changes{
+							&schema.DropSchema{
+								S: schema.New("market"),
+							},
+						},
+					},
+					{
+						Stmt: "CREATE DATABASE `market`",
+						Changes: schema.Changes{
+							&schema.AddSchema{
+								S: schema.New("market"),
+							},
+						},
+					},
+				},
+			},
+			Reporter: sqlcheck.ReportWriterFunc(func(r sqlcheck.Report) {
+				report = &r
+			}),
+		}
+	)
+	err := sqlcheck.Destructive.Analyze(context.Background(), pass)
+	require.NoError(t, err)
+	require.Equal(t, `Destructive changes detected in file 1.sql`, report.Text)
+	require.Len(t, report.Diagnostics, 2)
+	require.Equal(t, `Dropping non-empty schema "test" with 2 tables`, report.Diagnostics[0].Text)
+	require.Equal(t, `Dropping schema "market"`, report.Diagnostics[1].Text)
+}
+
+func TestDestructive_Register(t *testing.T) {
+	sqlcheck.Destructive.Register("mysql", func(ctx context.Context, p *sqlcheck.Pass) (diags []sqlcheck.Diagnostic, _ error) {
+		for _, cs := range p.File.Changes {
+			for _, c := range cs.Changes {
+				_, ok := c.(*schema.ModifySchema)
+				if !ok {
+					continue
+				}
+				// A fake driver-level diagnostic.
+				diags = append(diags, sqlcheck.Diagnostic{Text: "fake diagnostic", Pos: cs.Pos})
+			}
+		}
+		return
+	})
+	var (
+		report sqlcheck.Report
+		pass   = &sqlcheck.Pass{
+			Dev: &sqlclient.Client{Name: "mysql"},
+			File: &sqlcheck.File{
+				File: testFile{name: "1.sql"},
+				Changes: []*sqlcheck.Change{
+					{
+						Stmt: "ALTER TABLE `pets`",
+						Changes: []schema.Change{
+							&schema.ModifyTable{
+								T: schema.NewTable("pets").
+									SetSchema(schema.New("test")),
+								Changes: schema.Changes{
+									&schema.DropColumn{
+										C: schema.NewColumn("c").
+											SetGeneratedExpr(&schema.GeneratedExpr{Type: "STORED"}),
+									},
+								},
+							},
+						},
+					},
+					{
+						Stmt: "ALTER TABLE `pets`",
+						Changes: []schema.Change{
+							&schema.ModifySchema{
+								S: schema.New("test"),
+								Changes: schema.Changes{
+									&schema.ModifyAttr{
+										From: &schema.Charset{V: "utf8"},
+										To:   &schema.Charset{V: "latin1"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			Reporter: sqlcheck.ReportWriterFunc(func(r sqlcheck.Report) {
+				report = r
+			}),
+		}
+	)
+	err := sqlcheck.Destructive.Analyze(context.Background(), pass)
+	require.NoError(t, err)
+	require.Len(t, report.Diagnostics, 2)
+	require.Equal(t, "fake diagnostic", report.Diagnostics[0].Text)
+	require.Equal(t, `Dropping non-virtual column "c"`, report.Diagnostics[1].Text)
+}
+
+type testFile struct {
+	name string
+	migrate.File
+}
+
+func (t testFile) Name() string {
+	return t.name
+}
