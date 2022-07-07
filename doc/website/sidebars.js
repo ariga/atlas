f751762291e5b844070b97a2415872e2a65e09a0
Ariel Mashraki <ariel@mashraki.co.il>
2022-07-07 10:49:14 +0300

    doc: applying per driver

--- doc/website/sidebars.js
+++ doc/website/sidebars.js
@@ -78,14 +78,4 @@ module.exports = {
             id: 'about',
         }
     ]
-    // But you can create a sidebar manually
-    /*
-    tutorialSidebar: [
-      {
-        type: 'category',
-        label: 'Tutorial',
-        items: ['hello'],
-      },
-    ],
-     */
 };
