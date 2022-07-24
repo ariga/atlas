d8d2482bb0031a06fd9088a4422be5d22adda8dd
Ariel Mashraki <7413593+a8m@users.noreply.github.com>
2022-07-24 21:34:59 +0300

    cmd/atlas: extend error columns in revisions table to text (#981)

--- cmd/atlas/internal/migrate/ent/migrate/schema.go
+++ cmd/atlas/internal/migrate/ent/migrate/schema.go
@@ -21,7 +21,7 @@ var (
 		{Name: "total", Type: field.TypeInt},
 		{Name: "executed_at", Type: field.TypeTime},
 		{Name: "execution_time", Type: field.TypeInt64},
-		{Name: "error", Type: field.TypeString, Nullable: true},
+		{Name: "error", Type: field.TypeString, Nullable: true, Size: 2147483647},
 		{Name: "hash", Type: field.TypeString},
 		{Name: "partial_hashes", Type: field.TypeJSON, Nullable: true},
 		{Name: "operator_version", Type: field.TypeString},
