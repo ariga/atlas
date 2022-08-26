6dae774539fd84bbb4d72fe4a83734a03879bd31
Jannik Clausen <12862103+masseelch@users.noreply.github.com>
2022-08-26 10:56:48 +0200

    sql/migrate: support mysql comments marked with # (#1087)

--- sql/migrate/testdata/lex/6_skip_comment.sql
+++ sql/migrate/testdata/lex/6_skip_comment.sql
@@ -1,10 +1,14 @@
 -- comment 1
 CREATE TABLE t1(id int);
+# CREATE TABLE t1(id int);
 
 -- comment 2
+# CREATE TABLE t2(id int);
 CREATE TABLE t2(id int);
 -- comment 3
 CREATE TABLE t3(id int);
+
+# CREATE TABLE t3(id int);
 
 /* comment 4 */
 CREATE TABLE t4(
