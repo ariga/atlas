0929ec1cef5f1df26f35076683b58bb808bc9675
Pritesh Solanki <112630201+priteshtm@users.noreply.github.com>
2022-09-20 15:35:08 +0530

    doc/md/knowledge: added article "partial indexes in postgres" (#1148)

--- doc/website/sidebars.js
+++ doc/website/sidebars.js
@@ -109,6 +109,11 @@ module.exports = {
                     id: 'knowledge/postgres/serial-columns',
                     label: 'Serial Type Columns'
                 },
+                {
+                    type: 'doc',
+                    id: 'knowledge/postgres/partial-indexes',
+                    label: 'Partial Indexes'
+                },
             ],
         },
         {
