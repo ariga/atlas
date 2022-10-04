2cbfbdee940262bd5d762186654ea0cd38e53283
Travis Cline <travis.cline@gmail.com>
2022-10-03 18:23:00 -0700

    sql/spanner: Adopt new api in sqlspec

--- sql/spanner/sqlspec.go
+++ sql/spanner/sqlspec.go
@@ -15,6 +15,7 @@ import (
 	"ariga.io/atlas/sql/schema"
 	"ariga.io/atlas/sql/sqlspec"
 	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/zclconf/go-cty/cty"
 )
 
 type doc struct {
@@ -23,7 +24,7 @@ type doc struct {
 }
 
 // evalSpec evaluates an Atlas DDL document using an unmarshaler into v by using the input.
-func evalSpec(p *hclparse.Parser, v any, input map[string]string) error {
+func evalSpec(p *hclparse.Parser, v any, input map[string]cty.Value) error {
 	var d doc
 	if err := hclState.Eval(p, &d, input); err != nil {
 		return err
