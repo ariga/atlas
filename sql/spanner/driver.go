eb94b4bbfb1f555bd07ccdbe6af61f780ce2e78e
Travis Cline <travis.cline@gmail.com>
2022-10-06 12:52:19 -0700

    sql/spanner: Add start of spanner driver, implement basics of inspection (#1171)
    * cmd/atlas/internal/cmdapi: vercheck for updates (#1167)
    
    * schemahcl: support dynamic resource blocks
    
    * schemahcl: accept list(T) as inputs
    
    Also, use the native cty.Value type in our codebase
    
    * sql/spanner: Add start of spanner driver, implement basics of inspection
    
    * go.mod: Run tidy
    
    * spanner: Include header
    
    * sql/spanner: Comment import
    
    * spanner: Cleanup Inspection Logic
    
    * spanner: Cleanup long names and error messages
    
    * spanner: Simplify type casing
    
    * spanner: Migrate spanner driver reg. to cmd/
    
    * go.mod: Clean up go.mods
    
    * sql/spanner: Adopt new api in sqlspec
    
    * sql/spanner: Drop unused type
    
    * sql/spanner: Add comment regarding default schema aliasing
    
    * sql/spanner: Lower timeout for inspection query connections
    
    * spanner: Shorten column desc names
    
    * spanner: Add index ordering
    
    * spanner: Add Index & part Attrs
    
    * spanner: Add Attrs to StringType for spanner
    
    * spanner: Add index !ok handler
    
    Co-authored-by: Ariel Mashraki <7413593+a8m@users.noreply.github.com>
    
    * spanner: Address nits
    
    * sql/spanner: Address small issues
    
    * sql/spanner: Address small issues
    
    * sql/spanner: Remove unimplemented types
    
    * spanner: Address Nits
    
    * spanner: Remove unused types / change errs
    
    * spanner: refactor addIndex call for conformity
    
    * spanner: Clean up Index Addition
    
    * spanner: remove unnused types
    
    * sql/spanner: Fix StringType references
    
    * sql/spanner: Fix MaxSize attr attachment
    
    Co-authored-by: Rotem Tamir <rotemtamir@gmail.com>
    Co-authored-by: Ariel Mashraki <ariel@mashraki.co.il>
    Co-authored-by: Fraser Isbester <28307321+Fraser-Isbester@users.noreply.github.com>
    Co-authored-by: Ariel Mashraki <7413593+a8m@users.noreply.github.com>

---
+++ sql/spanner/driver.go
@@ -0,0 +1,95 @@
+// Copyright 2021-present The Atlas Authors. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+package spanner
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
+	"ariga.io/atlas/sql/internal/sqlx"
+	"ariga.io/atlas/sql/migrate"
+	"ariga.io/atlas/sql/schema"
+	"ariga.io/atlas/sql/sqlclient"
+)
+
+type (
+	// Driver represents a Spanner driver for introspecting database schemas,
+	// generating diff between schema elements and apply migrations changes.
+	Driver struct {
+		conn
+		schema.Differ
+		schema.Inspector
+		migrate.PlanApplier
+	}
+
+	// database connection and its information.
+	conn struct {
+		schema.ExecQuerier
+		dialect string
+	}
+)
+
+func init() {
+	sqlclient.Register(
+		"spanner",
+		sqlclient.DriverOpener(Open),
+		sqlclient.RegisterCodec(MarshalHCL, EvalHCL),
+		sqlclient.RegisterURLParser(sqlclient.URLParserFunc(func(u *url.URL) *sqlclient.URL {
+			uc := &sqlclient.URL{URL: u, DSN: strings.TrimPrefix(u.String(), u.Scheme+"://")}
+			return uc
+		})),
+	)
+}
+
+// Open opens a new Spanner driver.
+func Open(db schema.ExecQuerier) (migrate.Driver, error) {
+	var (
+		c   = conn{ExecQuerier: db}
+		ctx = context.Background()
+	)
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	rows, err := db.QueryContext(ctx, paramsQuery)
+	if err != nil {
+		return nil, fmt.Errorf("spanner: query database options: %w", err)
+	}
+	if err := sqlx.ScanOne(rows, &c.dialect); err != nil {
+		return nil, fmt.Errorf("spanner: scan database options: %w", err)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("spanner: failed to execute query: %w", err)
+	}
+	return &Driver{
+		conn:      c,
+		Inspector: &inspect{c},
+	}, nil
+}
+
+// Lock implements the schema.Locker interface.
+func (d *Driver) Lock(_ context.Context, name string, timeout time.Duration) (schema.UnlockFunc, error) {
+	return func() error { return nil }, nil
+}
+
+// Standard column types (and their aliases) as defined by Spanner.
+const (
+	TypeString    = "STRING"
+	TypeArray     = "ARRAY"
+	TypeBytes     = "BYTES"
+	TypeInt64     = "INT64"
+	TypeBool      = "BOOL"
+	TypeFloat64   = "FLOAT64"
+	TypeNumeric   = "NUMERIC"
+	TypeTimestamp = "TIMESTAMP"
+	TypeDate      = "DATE"
+	TypeStruct    = "STRUCT"
+	TypeJSON      = "JSON"
+)
+
+const (
+	stored = "STORED"
+)
