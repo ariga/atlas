348bc68f9b4eb8ed13171efebcae9d6ae9c4d424
Fraser Isbester <28307321+Fraser-Isbester@users.noreply.github.com>
2022-10-20 20:24:30 -0700

    sql/spanner: sqlspec: Add parser

---
+++ sql/spanner/convert.go
@@ -0,0 +1,12 @@
+package spanner
+
+import (
+	"fmt"
+
+	"ariga.io/atlas/sql/schema"
+)
+
+// ParseType returns the schema.Type value represented by the given raw type.
+func ParseType(string) (schema.Type, error) {
+	return nil, fmt.Errorf("not implemented")
+}
