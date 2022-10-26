c6e00e3f94a7b34367467f0d739ad8666cabaefc
Fraser Isbester <28307321+Fraser-Isbester@users.noreply.github.com>
2022-10-25 19:06:27 -0700

    sql/spanner: Strip unnused methods

--- sql/spanner/migrate.go
+++ sql/spanner/migrate.go
@@ -67,10 +67,6 @@ func (s *state) plan(ctx context.Context, changes []schema.Change) (err error) {
 			err = s.addTable(ctx, c)
 		case *schema.DropTable:
 			err = s.dropTable(c)
-		case *schema.ModifyTable:
-			err = s.modifyTable(ctx, c)
-		case *schema.RenameTable:
-			s.renameTable(c)
 		case *schema.DropIndex:
 			s.dropIndexes(c.I.Table, c.I)
 		case *schema.DropForeignKey:
@@ -144,86 +140,6 @@ func (s *state) dropTable(drop *schema.DropTable) error {
 		Comment: fmt.Sprintf("drop %q table", drop.T.Name),
 	})
 	return nil
-}
-
-// modifyTable builds and executes the queries for bringing the table into its modified state.
-// If the modification contains changes that are not index creation/deletion or a simple column
-// addition, the changes are applied using a temporary table following the procedure mentioned
-func (s *state) modifyTable(ctx context.Context, modify *schema.ModifyTable) error {
-	if alterable(modify) {
-		return s.alterTable(modify)
-	}
-	s.skipFKs = true
-	newT := *modify.T
-	indexes := newT.Indexes
-	fks := newT.ForeignKeys
-	newT.Indexes = nil
-	newT.ForeignKeys = nil
-	newT.Name = "new_" + newT.Name
-
-	// Create a new table with a temporary name, and copy the existing rows to it.
-	if err := s.addTable(ctx, &schema.AddTable{T: &newT}); err != nil {
-		return err
-	}
-	if err := s.copyRows(modify.T, &newT, modify.Changes); err != nil {
-		return err
-	}
-
-	// TODO(tmc): either drop or fix-up FKs in the mix here.
-
-	// Drop the current table, create the new one, and copy the rows back.
-	s.append(&migrate.Change{
-		Cmd:     Build("DROP TABLE").Ident(modify.T.Name).String(),
-		Source:  modify,
-		Comment: fmt.Sprintf("drop %q table after copying rows", modify.T.Name),
-	})
-	// walk foreign keys and swap out prefixes.
-
-	newNewT := newT
-	newNewT.Name = modify.T.Name
-	if err := s.addTable(ctx, &schema.AddTable{T: &newNewT}); err != nil {
-		return err
-	}
-	var reverseChanges []schema.Change
-	for _, c := range modify.Changes {
-		reverseChanges = append(reverseChanges, c)
-	}
-
-	// TODO(tmc): Prepare the reverse changes.
-	if err := s.copyRows(&newT, &newNewT, reverseChanges); err != nil {
-		return err
-	}
-	/*
-		s.append(&migrate.Change{
-			Cmd:     Build("ALTER TABLE").Ident(newT.Name).P("RENAME TO").Ident(modify.T.Name).String(),
-			Source:  modify,
-			Comment: fmt.Sprintf("rename temporary table %q to %q", newT.Name, modify.T.Name),
-		})
-	*/
-	// TODO: do we need to prefix/re-prefix the indexes?
-	if err := s.addIndexes(modify.T, indexes...); err != nil {
-		return err
-	}
-	// TODO: do we need to prefix/re-prefix the fks
-	if err := s.addForeignKeys(modify.T, fks...); err != nil {
-		return err
-	}
-	// drop the intermediate table:
-	s.append(&migrate.Change{
-		Cmd:     Build("DROP TABLE").Ident(newT.Name).String(),
-		Source:  modify,
-		Comment: fmt.Sprintf("drop temporary table %q", newT.Name),
-	})
-	return nil
-}
-
-func (s *state) renameTable(c *schema.RenameTable) {
-	s.append(&migrate.Change{
-		Source:  c,
-		Comment: fmt.Sprintf("rename a table from %q to %q", c.From.Name, c.To.Name),
-		Cmd:     Build("ALTER TABLE").Table(c.From).P("RENAME TO").Table(c.To).String(),
-		Reverse: Build("ALTER TABLE").Table(c.To).P("RENAME TO").Table(c.From).String(),
-	})
 }
 
 func (s *state) column(b *sqlx.Builder, c *schema.Column) error {
@@ -364,143 +280,8 @@ func (s *state) fks(b *sqlx.Builder, fks ...*schema.ForeignKey) {
 	})
 }
 
-func (s *state) copyRows(from *schema.Table, to *schema.Table, changes []schema.Change) error {
-	var (
-		args       []any
-		fromC, toC []string
-	)
-	for _, column := range to.Columns {
-		// Skip generated columns in INSERT as they are computed.
-		if sqlx.Has(column.Attrs, &schema.GeneratedExpr{}) {
-			continue
-		}
-		// Find a change that associated with this column, if exists.
-		var change schema.Change
-		for i := range changes {
-			switch c := changes[i].(type) {
-			case *schema.AddColumn:
-				if c.C.Name != column.Name {
-					break
-				}
-				if change != nil {
-					return fmt.Errorf("duplicate changes for column: %q: %T, %T", column.Name, change, c)
-				}
-				change = changes[i]
-			case *schema.ModifyColumn:
-				if c.To.Name != column.Name {
-					break
-				}
-				if change != nil {
-					return fmt.Errorf("duplicate changes for column: %q: %T, %T", column.Name, change, c)
-				}
-				change = changes[i]
-			case *schema.DropColumn:
-				if c.C.Name == column.Name {
-					return fmt.Errorf("unexpected drop column: %q", column.Name)
-				}
-			}
-		}
-		switch change := change.(type) {
-		// We expect that new columns are added with DEFAULT/GENERATED
-		// values or defined as nullable if the table is not empty.
-		case *schema.AddColumn:
-		// Column modification requires special handling if it was
-		// converted from nullable to non-nullable with default value.
-		case *schema.ModifyColumn:
-			toC = append(toC, column.Name)
-			if !column.Type.Null && column.Default != nil && change.Change.Is(schema.ChangeNull|schema.ChangeDefault) {
-				fromC = append(fromC, fmt.Sprintf("IFNULL(`%[1]s`, ?) AS `%[1]s`", column.Name))
-				x, err := defaultValue(column)
-				if err != nil {
-					return err
-				}
-				args = append(args, x)
-			} else {
-				fromC = append(fromC, column.Name)
-			}
-		// Columns without changes should be transferred as-is.
-		case nil:
-			toC = append(toC, column.Name)
-			fromC = append(fromC, column.Name)
-		}
-	}
-	stmt := fmt.Sprintf("INSERT INTO `%s` (%s) SELECT %s FROM `%s`", to.Name, identComma(toC), identComma(fromC), from.Name)
-	s.append(&migrate.Change{
-		Cmd:     stmt,
-		Args:    args,
-		Comment: fmt.Sprintf("copy rows from old table %q to new temporary table %q", from.Name, to.Name),
-	})
-	return nil
-}
-
-// alterTable alters the table with the given changes. Assuming the changes are "alterable".
-func (s *state) alterTable(modify *schema.ModifyTable) error {
-	for _, change := range modify.Changes {
-		switch change := change.(type) {
-		case *schema.AddIndex:
-			if err := s.addIndexes(modify.T, change.I); err != nil {
-				return err
-			}
-		case *schema.DropIndex:
-			if err := s.dropIndexes(modify.T, change.I); err != nil {
-				return err
-			}
-		case *schema.RenameIndex:
-			if err := s.addIndexes(modify.T, change.To); err != nil {
-				return err
-			}
-			if err := s.dropIndexes(modify.T, change.From); err != nil {
-				return err
-			}
-		case *schema.AddColumn:
-			b := Build("ALTER TABLE").Ident(modify.T.Name)
-			r := b.Clone()
-			if err := s.column(b.P("ADD COLUMN"), change.C); err != nil {
-				return err
-			}
-			s.append(&migrate.Change{
-				Source:  change,
-				Cmd:     b.String(),
-				Reverse: r.P("DROP COLUMN").Ident(change.C.Name).String(),
-				Comment: fmt.Sprintf("add column %q to table: %q", change.C.Name, modify.T.Name),
-			})
-		case *schema.RenameColumn:
-			b := Build("ALTER TABLE").Ident(modify.T.Name).P("RENAME COLUMN")
-			r := b.Clone()
-			s.append(&migrate.Change{
-				Source:  change,
-				Cmd:     b.Ident(change.From.Name).P("TO").Ident(change.To.Name).String(),
-				Reverse: r.Ident(change.To.Name).P("TO").Ident(change.From.Name).String(),
-				Comment: fmt.Sprintf("rename a column from %q to %q", change.From.Name, change.To.Name),
-			})
-		default:
-			return fmt.Errorf("unexpected change in alter table: %T", change)
-		}
-	}
-	return nil
-}
-
 func (s *state) append(c *migrate.Change) {
 	s.Changes = append(s.Changes, c)
-}
-
-func alterable(modify *schema.ModifyTable) bool {
-	for _, change := range modify.Changes {
-		switch change := change.(type) {
-		case *schema.RenameColumn, *schema.RenameIndex, *schema.DropIndex, *schema.AddIndex:
-		case *schema.AddColumn:
-			if len(change.C.Indexes) > 0 || len(change.C.ForeignKeys) > 0 || change.C.Default != nil {
-				return false
-			}
-			// TODO(tmc): check if this is true for spanner
-			if x := (schema.GeneratedExpr{}); sqlx.Has(change.C.Attrs, &x) {
-				return false
-			}
-		default:
-			return false
-		}
-	}
-	return true
 }
 
 // checks writes the CHECK constraint to the builder.
@@ -531,16 +312,4 @@ func defaultValue(c *schema.Column) (string, error) {
 	default:
 		return "", fmt.Errorf("unexpected default value type: %T", x)
 	}
-}
-
-func identComma(c []string) string {
-	b := &sqlx.Builder{QuoteChar: '`'}
-	b.MapComma(c, func(i int, b *sqlx.Builder) {
-		if strings.ContainsRune(c[i], '`') {
-			b.WriteString(c[i])
-		} else {
-			b.Ident(c[i])
-		}
-	})
-	return b.String()
 }
