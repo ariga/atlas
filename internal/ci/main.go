f93fd21c685946d16da7e0e7954e8b502a77a17a
Fraser Isbester <28307321+Fraser-Isbester@users.noreply.github.com>
2022-10-28 13:48:20 -0700

    sql/spanner: Add initial parsing and diffing implementations (#1220)
    * sql/spanner: sqlspec: Add parser
    
    * sql/spanner: re-add dropped WithFormatter
    
    * internal/ci: Re-add 9020 as it iss needed for spanner integration tests
    
    Co-authored-by: Travis Cline <travis.cline@gmail.com>

--- internal/ci/main.go
+++ internal/ci/main.go
@@ -165,7 +165,7 @@ var (
 			Version: "spanner",
 			Image:   "gcr.io/cloud-spanner-emulator/emulator:1.4.6",
 			Regex:   "Spanner",
-			Ports:   []string{"9010:9010"},
+			Ports:   []string{"9010:9010", "9020:9020"},
 			Env:     []string{"SPANNER_EMULATOR_HOST: localhost:9010"},
 			RawSetup: `
       - uses: 'google-github-actions/setup-gcloud@v0'
