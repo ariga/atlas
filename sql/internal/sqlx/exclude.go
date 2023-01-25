589f08000cc918f8a7bef738f27d96c4eba006db
Ariel Mashraki <ariel@mashraki.co.il>
2023-01-25 17:04:21 +0200

    sql/internal/sqlx: fix typo in var names

--- sql/internal/sqlx/exclude.go
+++ sql/internal/sqlx/exclude.go
@@ -140,12 +140,12 @@ func excludeT(t *schema.Table, pattern string) (err error) {
 		}
 		return filepath.Match(pattern, fk.Symbol)
 	})
-	t.Attrs, err = filter(t.Attrs, func(c schema.Attr) (bool, error) {
-		c, ok := c.(*schema.Check)
+	t.Attrs, err = filter(t.Attrs, func(a schema.Attr) (bool, error) {
+		c, ok := a.(*schema.Check)
 		if !ok {
 			return false, nil
 		}
-		match, err := filepath.Match(pattern, t.Name)
+		match, err := filepath.Match(pattern, c.Name)
 		if !match || err != nil {
 			return false, err
 		}
