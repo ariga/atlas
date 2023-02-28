0c8baf4d13a335659feea84e3631bad22466e05d
Dmytro Velychko <dima@codempire.team>
2023-02-28 21:20:52 +0200

    doc: update update main page design

--- doc/website/src/pages/index.js
+++ doc/website/src/pages/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import LayoutProvider from "@theme/Layout/Provider";
-import BrowserOnly from "@docusaurus/core/lib/client/exports/BrowserOnly";
-import { AtlasWebsite } from "@ariga/atlas-website";
+import * as Atlas from "@ariga/atlas-website";
+
 import "@ariga/atlas-website/style.css";
 
 export default function () {
   return (
     <LayoutProvider>
-      <BrowserOnly>
-        {() => {
-          return <AtlasWebsite />;
-        }}
-      </BrowserOnly>
+      <Atlas.WebsiteWrapper>
+        <Atlas.BackgroundWrapper>
+          <Atlas.ContentWrapper>
+            <Atlas.Header />
+            <Atlas.Intro />
+            <Atlas.CompaniesDesktop />
+            <Atlas.UseCase />
+            <Atlas.TestimonialsTablet />
+            <Atlas.Guides />
+            <Atlas.CompaniesMobile />
+            <Atlas.Newsletter />
+            <Atlas.Discover />
+            <Atlas.Footer />
+          </Atlas.ContentWrapper>
+        </Atlas.BackgroundWrapper>
+      </Atlas.WebsiteWrapper>
     </LayoutProvider>
   );
 }
