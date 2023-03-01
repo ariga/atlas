423e4fc19c0a3d86eceab81551b7d5d999673e50
Rotem Tamir <rotemtamir@gmail.com>
2023-03-01 15:06:23 +0200

    cmd/atlas/internal/cloudapi: cloud client (#1493)

---
+++ cmd/atlas/internal/cloudapi/client.go
@@ -0,0 +1,81 @@
+// Copyright 2021-present The Atlas Authors. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+package cloudapi
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"ariga.io/atlas/sql/migrate"
+)
+
+const UserAgent = "atlas-cli"
+
+// Client is a client for the Atlas Cloud API.
+type Client struct {
+	client   gqlClient
+	endpoint string
+}
+
+// New creates a new Client for the Atlas Cloud API.
+func New(endpoint, token string) *Client {
+	return &Client{
+		client: newGQLClient(endpoint, &http.Client{
+			Transport: &roundTripper{
+				token: token,
+			},
+			Timeout: time.Second * 30,
+		}),
+	}
+}
+
+// DirInput is the input type for retrieving a single directory.
+type DirInput struct {
+	Name string `json:"name"`
+}
+
+// Dir retrieves a directory from the Atlas Cloud API.
+func (c *Client) Dir(ctx context.Context, input DirInput) (migrate.Dir, error) {
+	var payload struct {
+		Dir struct {
+			Content []byte `json:"content"`
+		} `json:"dir"`
+	}
+	if err := c.client.MakeRequest(
+		ctx,
+		&Request{
+			Query: `
+				query getDir($input: DirInput!) {
+					dir(input: $input) {
+						content
+					}
+				}`,
+			Variables: struct {
+				Input DirInput `json:"input"`
+			}{
+				Input: input,
+			},
+		},
+		&Response{
+			Data: &payload,
+		}); err != nil {
+		return nil, err
+	}
+	return migrate.UnarchiveDir(payload.Dir.Content)
+}
+
+// roundTripper is a http.RoundTripper that adds the Authorization header.
+type roundTripper struct {
+	token string
+}
+
+// RoundTrip implements http.RoundTripper.
+func (r *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("Authorization", "Bearer "+r.token)
+	req.Header.Set("User-Agent", UserAgent)
+	req.Header.Set("Content-Type", "application/json")
+	return http.DefaultTransport.RoundTrip(req)
+}
---
+++ cmd/atlas/internal/cloudapi/client_test.go
@@ -0,0 +1,42 @@
+// Copyright 2021-present The Atlas Authors. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+package cloudapi
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"ariga.io/atlas/sql/migrate"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClient(t *testing.T) {
+	var (
+		dir = migrate.MemDir{}
+		err = dir.WriteFile("1.sql", []byte("create table foo (id int)"))
+	)
+	require.NoError(t, err)
+	ad, err := migrate.ArchiveDir(&dir)
+	require.NoError(t, err)
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, "Bearer atlas", r.Header.Get("Authorization"))
+		require.Equal(t, UserAgent, r.Header.Get("User-Agent"))
+		fmt.Fprintf(w, `{"data":{"dir":{"content":%q}}}`, base64.StdEncoding.EncodeToString(ad))
+	}))
+	client := New(srv.URL, "atlas")
+	defer srv.Close()
+	gd, err := client.Dir(context.Background(), DirInput{
+		Name: "foo",
+	})
+	gcheck, err := gd.Checksum()
+	require.NoError(t, err)
+	dcheck, err := dir.Checksum()
+	require.NoError(t, err)
+	require.Equal(t, dcheck.Sum(), gcheck.Sum())
+}
