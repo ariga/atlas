26c5c507b9005043a3d3369fbd4260ec36675269
Dima Velychko <dima@codempire.team>
2023-04-16 10:23:56 +0300

    feature/atlas-cloud-link (#1564)

--- doc/website/src/pages/index.js
+++ doc/website/src/pages/index.js
@@ -1,28 +1,13 @@
 import React from "react";
 import LayoutProvider from "@theme/Layout/Provider";
-import * as Atlas from "@ariga/atlas-website";
+import { AtlasGoWebsite } from "@ariga/atlas-website";
 
 import "@ariga/atlas-website/style.css";
 
 export default function () {
   return (
     <LayoutProvider>
-      <Atlas.WebsiteWrapper>
-        <Atlas.BackgroundWrapper>
-          <Atlas.ContentWrapper>
-            <Atlas.Header />
-            <Atlas.Intro />
-            <Atlas.CompaniesDesktop />
-            <Atlas.UseCase />
-            <Atlas.TestimonialsTablet />
-            <Atlas.Guides />
-            <Atlas.CompaniesMobile />
-            <Atlas.Newsletter />
-            <Atlas.Discover />
-            <Atlas.Footer />
-          </Atlas.ContentWrapper>
-        </Atlas.BackgroundWrapper>
-      </Atlas.WebsiteWrapper>
+      <AtlasGoWebsite />
     </LayoutProvider>
   );
 }
