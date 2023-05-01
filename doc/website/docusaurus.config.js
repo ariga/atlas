01cb833e7b728251d9d8cb573c4c2bd637c0c84a
Shani <117578627+shani-a@users.noreply.github.com>
2023-05-01 10:33:36 +0300

    doc/website - add Atals cloud to atlas website footer (#1615)
    * add atals cloud to atlas website
    
    * Apply suggestions from code review
    
    Co-authored-by: Hila Kashai <73284641+hilakashai@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Hila Kashai <73284641+hilakashai@users.noreply.github.com>

--- doc/website/docusaurus.config.js
+++ doc/website/docusaurus.config.js
@@ -110,12 +110,20 @@ module.exports = {
           ]
         },
         {
+          "title": "Atlas Cloud",
+          "items": [
+            {"label": "Discover Atlas Cloud", "to": "https://atlasgo.cloud/"},
+            {"label": "Live Demo", "to": "https://gh.atlasgo.cloud/dirs"},
+            {"label": "Sign Up", "to": "https://auth.atlasgo.cloud/signup"},
+          ]
+        },
+        {
           "title": "Legal",
           "items": [
             {"label": "Privacy Policy", "to": "https://ariga.io/legal/privacy"},
             {"label": "Terms of Service", "to": "https://ariga.io/legal/tos"},
           ]
-        }
+        },
       ],
       copyright: `
       Copyright © ${new Date().getFullYear()} The Atlas Authors.
