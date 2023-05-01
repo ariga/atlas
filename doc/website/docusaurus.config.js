451a218974dddcb82d4a06853fe6f42920c198e1
Shani <117578627+shani-a@users.noreply.github.com>
2023-05-01 10:12:42 +0300

    Apply suggestions from code review
    Co-authored-by: Hila Kashai <73284641+hilakashai@users.noreply.github.com>

--- doc/website/docusaurus.config.js
+++ doc/website/docusaurus.config.js
@@ -112,8 +112,8 @@ module.exports = {
         {
           "title": "Atlas Cloud",
           "items": [
-            {"label": "Meet Atlas Cloud", "to": "https://atlasgo.cloud/"},
-            {"label": "Live demo", "to": "https://gh.atlasgo.cloud/dirs"},
+            {"label": "Discover Atlas Cloud", "to": "https://atlasgo.cloud/"},
+            {"label": "Live Demo", "to": "https://gh.atlasgo.cloud/dirs"},
             {"label": "Sign Up", "to": "https://auth.atlasgo.cloud/signup"},
           ]
         },
@@ -124,9 +124,6 @@ module.exports = {
             {"label": "Terms of Service", "to": "https://ariga.io/legal/tos"},
           ]
         },
-
-
-
       ],
       copyright: `
       Copyright © ${new Date().getFullYear()} The Atlas Authors.
