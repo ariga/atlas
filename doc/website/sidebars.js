c2c2e37d2e9f806e3a7f956eb6b7b2f909918cef
Rotem Tamir <rotemtamir@gmail.com>
2023-05-07 10:44:41 +0300

    doc/md: kubernetes operator docs

--- doc/website/sidebars.js
+++ doc/website/sidebars.js
@@ -77,6 +77,7 @@ module.exports = {
             label: 'Integrations',
             collapsed: false,
             items: [
+                {type: 'doc', id: 'integrations/kubernetes-operator', label: 'Kubernetes Operator'},
                 {type: 'doc', id: 'integrations/github-actions', label: 'GitHub Actions'},
                 {type: 'doc', id: 'integrations/terraform-provider', label: 'Terraform Provider'},
                 {type: 'doc', id: 'integrations/go-api', label: 'Go API'},
