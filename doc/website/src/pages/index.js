5721841dcb85cf81754f2fc15363f01a40e449fd
Rotem Tamir <rotemtamir@gmail.com>
2023-06-01 10:53:25 +0300

    doc/website: adding events (#1705)

--- doc/website/src/pages/index.js
+++ doc/website/src/pages/index.js
@@ -7,7 +7,27 @@ import "@ariga/atlas-website/style.css";
 export default function () {
   return (
     <LayoutProvider>
-      <AtlasGoWebsite />
+      <AtlasGoWebsite
+        events={{
+          events: [
+              {
+                  title: "CI/CD for Databases",
+                  date: "Monday, June 12th, 2023. 13:00 UTC",
+                  description: "Learn how to use Atlas Cloud to prevent risky " +
+                      "database changes and apply CD for schema changes with Terraform and Kubernetes.",
+                  linkUrl: "http://26565461.hs-sites-eu1.com/webinar-ci/cd-for-databases-with-atlas-cloud",
+                  imageUrl: "https://atlasgo.io/uploads/event-placeholder.png",
+              },
+              {
+                  title: "Getting started with Atlas Cloud",
+                  date: "COMING SOON",
+                  description: "Join us to learn how to set up Atlas Cloud for your team in under one minute.",
+                  imageUrl: "https://atlasgo.io/uploads/event-placeholder.png",
+              },
+            ],
+            isHidden: false,
+        }}
+      />
     </LayoutProvider>
   );
 }
