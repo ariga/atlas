927285d980f6bc6ea49570c2230f97b6ba59f766
Jannik Clausen <12862103+masseelch@users.noreply.github.com>
2023-06-21 14:24:14 +0200

    cmd/atlas/internal/cmdapi: add flavor to version (#1770)

---
+++ cmd/atlas/internal/cmdapi/version_oss.go
@@ -0,0 +1,9 @@
+// Copyright 2021-present The Atlas Authors. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+//go:build !ent
+
+package cmdapi
+
+var flavor = "atlas community"
