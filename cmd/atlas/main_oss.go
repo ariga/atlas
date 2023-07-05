b1f6c6a28cd51298f82fc5b5f40e4dbc8fc00ea0
Jannik Clausen <12862103+masseelch@users.noreply.github.com>
2023-07-05 12:42:54 +0200

    cmd/atlas: create context per flavor (#1821)

---
+++ cmd/atlas/main_oss.go
@@ -0,0 +1,18 @@
+// Copyright 2021-present The Atlas Authors. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+//go:build !ent
+
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+)
+
+func newContext() context.Context {
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	return ctx
+}
