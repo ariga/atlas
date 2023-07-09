6c5f9ab7216be3fb2e2dd90f4c7a64cadef3a8c9
Shani <117578627+shani-a@users.noreply.github.com>
2023-07-09 14:32:59 +0300

    doc/website: update upcoming events (#1835)

--- doc/website/src/pages/index.js
+++ doc/website/src/pages/index.js
@@ -11,10 +11,21 @@ export default function () {
         events={{
           events: [
               {
-                  title: "Getting started with Atlas Cloud",
-                  date: "COMING SOON",
-                  description: "Join us to learn how to set up Atlas Cloud for your team in under one minute.",
-                  imageUrl: "https://atlasgo.io/uploads/event-placeholder.png",
+                  title: "HashiTalks 2023",
+                  date: "Thursday, July 13th, 2023. 08:05 UTC",
+                  description: "Join our lecture on HashiTalks:Israel 2023 and learn how to build modern schema change " +
+                                "management workflows using Terraform, Vault, and Atlas to sustain fast and safe schema evolution.",
+                  linkUrl: 'https://events.hashicorp.com/hashitalksisrael',
+                  imageUrl: 'https://atlasgo.io/uploads/images/posts/v0.3.2/hashitialks2023_3.png',
+                  linkText: 'Register',
+              },
+              {
+                  title: "Kubernetes-native schema migrations",  
+                  date: "Wednesday, July 19th, 2023. 13:00 UTC",
+                  description: "Discover the power of the Atlas Kubernetes Operator for seamless management of your database schemas in Kubernetes.",
+                  linkUrl: 'https://26565461.hs-sites-eu1.com/webinar-ci/cd-for-databases-with-atlas-cloud-0-0',
+                  imageUrl: 'https://atlasgo.io/uploads/images/posts/v0.3.2/kubernetes_webinar3.png',
+                  linkText: 'Register',
               },
             ],
             isHidden: false,
@@ -23,3 +34,4 @@ export default function () {
     </LayoutProvider>
   );
 }
+
