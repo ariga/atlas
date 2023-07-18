ae9b968d6a41913068f4107453b5bf2ff86dadca
Shani <shani.cohen@ariga.io>
2023-07-18 13:44:42 +0300

    atlas/src: update k8s webinar date

--- doc/website/src/pages/index.js
+++ doc/website/src/pages/index.js
@@ -12,7 +12,7 @@ export default function () {
           events: [
               {
                   title: "Kubernetes-native schema migrations",  
-                  date: "Wednesday, July 19th, 2023. 13:00 UTC",
+                  date: "Wednesday, August 2nd, 2023, 13:00 UTC",
                   description: "Discover the power of the Atlas Kubernetes Operator for seamless management of your database schemas in Kubernetes.",
                   linkUrl: 'http://26565461.hs-sites-eu1.com/kubernetes-native-schema-migrations-webinar',
                   imageUrl: 'https://atlasgo.io/uploads/images/posts/v0.3.2/kubernetes_webinar3.png',
