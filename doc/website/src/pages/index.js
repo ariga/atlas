33c5a221ef11f52a324d56fb8d7e0b9c35c3fffa
Shani <117578627+shani-a@users.noreply.github.com>
2023-07-20 19:30:49 +0300

    doc/website: update utm tags to k8s event (#1884)

--- doc/website/src/pages/index.js
+++ doc/website/src/pages/index.js
@@ -14,7 +14,7 @@ export default function () {
                   title: "Kubernetes-native schema migrations",  
                   date: "Wednesday, August 2nd, 2023, 13:00 UTC",
                   description: "Discover the power of the Atlas Kubernetes Operator for seamless management of your database schemas in Kubernetes.",
-                  linkUrl: 'https://page.ariga.io/kubernetes-native-schema-migrations-webinar',
+                  linkUrl: 'https://page.ariga.io/kubernetes-native-schema-migrations-webinar?utm_source=atlasgo&utm_medium=website&utm_term=events',
                   imageUrl: 'https://atlasgo.io/uploads/images/posts/v0.3.2/kubernetes_webinar3.png',
                   linkText: 'Register',
               },
