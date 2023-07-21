f48a8cf7021d3911e3c128f08f1955c5ecc185a5
Ariel Mashraki <ariel@mashraki.co.il>
2023-07-21 17:47:20 +0300

    sql/sqlite: add stubs for views operations

---
+++ sql/sqlite/driver_oss.go
@@ -0,0 +1,33 @@
+// Copyright 2021-present The Atlas Authors. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+//go:build !ent
+
+package sqlite
+
+import (
+	"context"
+
+	"ariga.io/atlas/sql/schema"
+)
+
+func (*inspect) inspectViews(context.Context, *schema.Realm, *schema.InspectOptions) error {
+	return nil // unimplemented.
+}
+
+func (*state) addView(*schema.AddView) error {
+	return nil // unimplemented.
+}
+
+func (*state) dropView(*schema.DropView) error {
+	return nil // unimplemented.
+}
+
+func (*state) modifyView(*schema.ModifyView) error {
+	return nil // unimplemented.
+}
+
+func (*state) renameView(*schema.RenameView) error {
+	return nil // unimplemented.
+}
