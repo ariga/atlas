d8b88c2e7acb06af4d37109fae88d6083b8aa179
Ariel Mashraki <ariel@mashraki.co.il>
2023-11-06 23:18:12 +0200

    cmd/atlas/internal/cmdext: stub for remote_schema data source

--- cmd/atlas/internal/cmdext/cmdext_oss.go
+++ cmd/atlas/internal/cmdext/cmdext_oss.go
@@ -9,7 +9,16 @@ package cmdext
 import (
 	"context"
 	"fmt"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/zclconf/go-cty/cty"
 )
+
+// RemoteSchema is a data source that for reading remote schemas.
+func RemoteSchema(ctx *hcl.EvalContext, block *hclsyntax.Block) (cty.Value, error) {
+	return cty.Zero, fmt.Errorf("data.remote_schema is not supported by this release. See: https://atlasgo.io/getting-started")
+}
 
 // StateReaderAtlas returns a migrate.StateReader from an Atlas Cloud schema.
 func StateReaderAtlas(context.Context, *StateReaderConfig) (*StateReadCloser, error) {
