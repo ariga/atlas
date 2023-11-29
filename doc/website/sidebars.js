216fd83c467420a4a91994f0759bb0f3f154b79d
Rotem Tamir <rotemtamir@gmail.com>
2023-11-29 22:17:05 +0200

    doc/md/guides: documenting working with secrets (#2320)

--- doc/website/sidebars.js
+++ doc/website/sidebars.js
@@ -203,6 +203,11 @@ module.exports = {
                     type: 'doc',
                     id: 'guides/deploying/cloud-sql-via-github-actions',
                     label: 'GCP CloudSQL (GH Actions)'
+                },
+                {
+                    type: 'doc',
+                    id: 'guides/deploying/secrets',
+                    label: 'Working with secrets'
                 }
             ]
         },
