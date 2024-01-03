2a41bb6878609c2565a23bc258fc2b1e0688852b
Ariel Mashraki <ariel@mashraki.co.il>
2024-01-03 10:18:36 +0200

    cmd/atlas/internal/sqlparse: rename myparse.Parser to FileParser

--- cmd/atlas/internal/sqlparse/myparse/myparse_oss.go
+++ cmd/atlas/internal/sqlparse/myparse/myparse_oss.go
@@ -14,19 +14,19 @@ import (
 )
 
 // Parser for fixing linting changes.
-type Parser struct{}
+type FileParser struct{}
 
 // FixChange fixes the changes according to the given statement.
-func (*Parser) FixChange(migrate.Driver, string, schema.Changes) (schema.Changes, error) {
+func (*FileParser) FixChange(migrate.Driver, string, schema.Changes) (schema.Changes, error) {
 	return nil, errors.New("unimplemented")
 }
 
 // ColumnFilledBefore checks if the column was filled with values before the given position in the file.
-func (*Parser) ColumnFilledBefore([]*migrate.Stmt, *schema.Table, *schema.Column, int) (bool, error) {
+func (*FileParser) ColumnFilledBefore([]*migrate.Stmt, *schema.Table, *schema.Column, int) (bool, error) {
 	return false, errors.New("unimplemented")
 }
 
 // CreateViewAfter checks if a view was created after the position with the given name to a table.
-func (*Parser) CreateViewAfter([]*migrate.Stmt, string, string, int) (bool, error) {
+func (*FileParser) CreateViewAfter([]*migrate.Stmt, string, string, int) (bool, error) {
 	return false, errors.New("unimplemented")
 }
