79b22a79f4cf89db1f2111d180d2e6849d8805de
Rotem Tamir <rotemtamir@gmail.com>
2024-02-14 12:21:30 +0200

    cmd/atlas: rm gen

--- cmd/atlas/generate.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2021-present The Atlas Authors. All rights reserved.
-// This source code is licensed under the Apache 2.0 license found
-// in the LICENSE file in the root directory of this source tree.
-
-package main
-
-//go:generate go run doc.go
