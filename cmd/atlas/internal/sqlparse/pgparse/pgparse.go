d6cf3728412ec62f3afde0b3efec729e50291a62
Ariel Mashraki <ariel@mashraki.co.il>
2024-04-21 16:49:51 +0300

    cmd/atlas: use pgquery for windows

--- cmd/atlas/internal/sqlparse/pgparse/pgparse.go
+++ cmd/atlas/internal/sqlparse/pgparse/pgparse.go
@@ -2,8 +2,6 @@
 // This source code is licensed under the Apache 2.0 license found
 // in the LICENSE file in the root directory of this source tree.
 
-//go:build !windows
-
 package pgparse
 
 import (
