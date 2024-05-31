ff249fbbece0b40b4e401af87b246349493b09c7
Giau. Tran Minh <12751435+giautm@users.noreply.github.com>
2024-05-31 23:01:06 +0700

    sql/sqlspec: Revert support `name` attribute on the table resource (#2820) (#2822)
    This reverts commit f8a15b3aa05455cac4de72fd30bcc6c07969d2e5.

--- internal/integration/hclsqlspec/hclsqlspec_test.go
+++ internal/integration/hclsqlspec/hclsqlspec_test.go
@@ -371,10 +371,6 @@ schema "account_b" {
 table "t2" {
 	schema = schema.account_b
 }
-table "t3" {
-	schema = schema.account_b
-	name = "foo"
-}
 `
 
 	for _, tt := range dialects {
@@ -396,14 +392,12 @@ table "t3" {
 						Realm: &r,
 						Tables: []*schema.Table{
 							{Name: "t2"},
-							{Name: "foo"},
 						},
 					},
 				},
 			}
 			exp.Schemas[0].Tables[0].Schema = exp.Schemas[0]
 			exp.Schemas[1].Tables[0].Schema = exp.Schemas[1]
-			exp.Schemas[1].Tables[1].Schema = exp.Schemas[1]
 			require.EqualValues(t, exp, &r)
 			hcl, err := tt.MarshalSpec(&r)
 			require.NoError(t, err)
