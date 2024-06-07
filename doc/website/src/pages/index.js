c5a4863cde81be527e86f890eef49bdc7f102145
Dima Velychko <velychko.dmitriy@gmail.com>
2024-06-07 17:29:52 +0300

    doc/unify-footer

--- doc/website/src/pages/index.js
+++ doc/website/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import LayoutProvider from "@theme/Layout/Provider";
 import { AtlasGoWebsite } from "@ariga/atlas-website";
+import FOOTER from "../constants/footer";
 
 import "@ariga/atlas-website/style.css";
 
@@ -9,13 +10,12 @@ export default function () {
     <LayoutProvider>
       <AtlasGoWebsite
         events={{
-          events: [
-            ],
-            isHidden: true,
+          events: [],
+          isHidden: true,
         }}
         projectsAmount={3000}
+        footer={FOOTER}
       />
     </LayoutProvider>
   );
 }
-
