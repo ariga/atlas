40269aa6e659e1a3db9369104d8189d18f19160a
Dima Velychko <velychko.dmitriy@gmail.com>
2024-06-09 09:23:37 +0300

    doc/website: unify-footer (#2848)

--- doc/website/docusaurus.config.js
+++ doc/website/docusaurus.config.js
@@ -1,3 +1,5 @@
+const footer = require('./src/constants/footer');
+
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'Atlas | Manage your database schema as code',
@@ -89,64 +91,7 @@ module.exports = {
         },
       ],
     },
-    //
-    "footer": {
-      "links": [
-        {
-          "title": "Docs",
-          "items": [
-            {"label": "Home", "to": "docs"},
-            {"label": "Atlas vs Others ", "to": "atlas-vs-others"},
-            {"label": "CLI Reference", "to": "cli-reference"},
-            {"label": "Blog", "to": "blog"},
-            {"label": "Guides", "to": "guides"},
-            {"label": "GoDoc", "to": "https://pkg.go.dev/ariga.io/atlas"},
-          ]
-        },
-        {
-          "title": "Community",
-          "items": [
-            {"label": "GitHub", "to": "https://github.com/ariga/atlas"},
-            {"label": "Discord", "to": "https://discord.gg/zZ6sWVg6NT"},
-            {"label": "Twitter", "to": "https://twitter.com/ariga_io"},
-            {"label": "Newsletter", "to": "https://atlasnewsletter.substack.com/"},
-            {"label": "YouTube", "to": "https://youtube.com/@ariga_io"}
-          ]
-        },
-        {
-          "title": "Integrations",
-          "items": [
-            {"label": "GitHub Actions", "to": "/integrations/github-actions"},
-            {"label": "Kubernetes Operator", "to": "/integrations/kubernetes/operator"},
-            {"label": "Terraform", "to": "/integrations/terraform-provider"},
-            {"label": "Go API", "to": "/integrations/go-api"}
-          ]
-        },
-        {
-          "title": "Atlas Cloud",
-          "items": [
-            {"label": "Discover Atlas Cloud", "to": "https://atlasgo.cloud/?utm_term=footer"},
-            {"label": "Live Demo", "to": "https://gh.atlasgo.cloud/projects?utm_term=footer"},
-            {"label": "Sign Up", "to": "https://auth.atlasgo.cloud/signup?utm_term=footer"},
-          ]
-        },
-        {
-          "title": "Legal",
-          "items": [
-            {"label": "Privacy Policy", "to": "https://ariga.io/legal/privacy"},
-            {"label": "Terms of Service", "to": "https://ariga.io/legal/tos"},
-          ]
-        },
-      ],
-      copyright: `
-      Copyright © ${new Date().getFullYear()} The Atlas Authors.
-      The Go gopher was designed by <a href="http://reneefrench.blogspot.com/">Renee French</a>.
-      <br/>
-      The design for the Go gopher is licensed under the Creative Commons 3.0 Attributions license. Read this 
-      <a href="https://blog.golang.org/gopher">article</a> for more details.
-      <br/>
-      `,
-    },
+    footer,
     announcementBar: {
       id: 'announcementBar-13', // Increment on change
       content: `<strong>Atlas v0.23: Redshift, CircleCI, GORM Views, and more. </strong><a class="cta" href="https://atlasgo.io/blog/2024/05/21/atlas-v-0-23">Read it here</a>`,
