bc35c33893f02378b7621551bd2d6a06d68a84c2
Dima Velychko <velychko.dmitriy@gmail.com>
2024-06-18 16:26:13 +0300

    doc/home-announcement-bar

--- doc/website/src/pages/index.js
+++ doc/website/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LayoutProvider from "@theme/Layout/Provider";
+import AnnouncementBar from "@theme/AnnouncementBar";
 import { AtlasGoWebsite } from "@ariga/atlas-website";
 import FOOTER from "../constants/footer";
 
@@ -8,6 +9,7 @@ import "@ariga/atlas-website/style.css";
 export default function () {
   return (
     <LayoutProvider>
+      <AnnouncementBar />
       <AtlasGoWebsite
         events={{
           events: [],
