e7aa355e4500eed989f30c336de863467486b0a3
Hila Kashai <hila@ariga.io>
2024-06-20 14:32:11 +0300

    doc/md: add deployments doc

--- doc/website/sidebars.js
+++ doc/website/sidebars.js
@@ -125,6 +125,8 @@ module.exports = {
                         {type: 'doc', id: 'cloud/features/registry', label: 'Schema Registry'},
                         {type: 'doc', id: 'cloud/features/schema-docs', label: 'Schema Docs'},
                         {type: 'doc', id: 'cloud/features/pre-migration-checks', label: 'Pre-migration Checks'},
+                        {type: 'doc', id: 'cloud/features/migration-troubleshoot', label: 'Migration Troubleshooting'},
+
                     ],
                 },
                 {type: 'doc', id: 'cloud/pricing', label: 'Pricing'},
