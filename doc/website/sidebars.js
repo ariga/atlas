7a2ae96875f4f9040717b7758ef72ba2427ede4d
Tran Minh Luan <tranminhluan1206@gmail.com>
2024-08-08 17:10:31 +0700

    doc/md/guides: add Guide for EF Core (#3023)
    * doc/md/guides: add Card for EF Core guide
    
    * doc/md: add efcore as External Schema
    
    * doc/md/guides: add Guide for EF Core
    
    * doc/md/guides: fix grammars
    
    * doc/md/guides: add instruction to run Atlas Windows
    
    * chore: fix typo
    
    Co-authored-by: Hila Kashai <73284641+hilakashai@users.noreply.github.com>
    
    * doc/md/guides: edit Windows instruction
    
    * doc/md/guides: emphasize the need of using atlas
    
    * doc/md/guides: fix typo
    
    * fixes
    
    * doc/md/guides: add Verifying the Configuration
    
    * doc/md/guides: fix typo entity -> model
    
    ---------
    
    Co-authored-by: Hila Kashai <73284641+hilakashai@users.noreply.github.com>
    Co-authored-by: Rotem Tamir <rotemtamir@gmail.com>

--- doc/website/sidebars.js
+++ doc/website/sidebars.js
@@ -464,6 +464,11 @@ module.exports = {
                     type: 'doc',
                     id: 'guides/frameworks/sqlc-versioned',
                     label: 'sqlc (versioned)'
+                },
+                {
+                    type: 'doc',
+                    id: 'guides/orms/efcore',
+                    label: 'Entity Framework Core'
                 }
             ]
         },
