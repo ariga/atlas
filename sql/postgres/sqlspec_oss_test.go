ca25847069214a01dd18e0a26320c7177739ab1b
Ariel Mashraki <7413593+a8m@users.noreply.github.com>
2024-09-24 12:05:17 +0300

    sql/postgres: reduce usage of stubs and separate oss/ent builds (#3146)

--- sql/postgres/sqlspec_oss_test.go
+++ sql/postgres/sqlspec_oss_test.go
@@ -2,6 +2,8 @@
 // This source code is licensed under the Apache 2.0 license found
 // in the LICENSE file in the root directory of this source tree.
 
+//go:build !ent
+
 package postgres
 
 import (
