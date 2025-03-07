c8607516c12355690bccea9f26ccf668f0ed322d
Ariel Mashraki <7413593+a8m@users.noreply.github.com>
2025-03-07 12:31:59 +0200

    sql/internal/sqlx: add ViewDepT option to SortChanges (#3417)

--- sql/internal/sqlx/plan.go
+++ sql/internal/sqlx/plan.go
@@ -390,6 +390,8 @@ type SortOptions struct {
 	FuncDepV func(*schema.Func, *schema.View) bool
 	// FuncDepO reports if a function depends on the given object.
 	FuncDepO func(*schema.Func, schema.Object) bool
+	// ViewDepT reports if a view depends on the given table.
+	ViewDepT func(*schema.View, *schema.Table) bool
 	// CompareFuncArgs set to true to compare function arguments.
 	CompareFuncArgs bool
 	// DefaultSchema defines the default schema (also known as "search_path") that
