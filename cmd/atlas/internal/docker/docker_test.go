3bf015bd2a664d33efadee6749aa22d8c14e236f
Giau. Tran Minh <12751435+giautm@users.noreply.github.com>
2025-05-12 20:16:14 +0700

    cmd/atlas/docker: Revert let Docker Engine allocate a free port (#3500)
    This reverts commit 25065bcac52c9d9952cd59bde4607bcce150e927.

--- cmd/atlas/internal/docker/docker_test.go
+++ cmd/atlas/internal/docker/docker_test.go
@@ -6,7 +6,6 @@ package docker
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"net/url"
 	"testing"
@@ -19,7 +18,7 @@ func TestDockerConfig(t *testing.T) {
 
 	// invalid config
 	_, err := (&Config{}).Run(ctx)
-	require.ErrorContains(t, err, fmt.Sprintf("invalid configuration %#v", &Config{}))
+	require.Error(t, err)
 
 	// MySQL
 	cfg, err := MySQL("latest", Out(io.Discard))
@@ -469,13 +468,11 @@ func TestContainerURL(t *testing.T) {
 			driver: "postgres",
 			User:   url.UserPassword("postgres", "pass"),
 		},
-		HostPort: "0.0.0.0:5432",
+		Port: "5432",
 	}
-	// Without DOCKER_HOST
-	t.Setenv("DOCKER_HOST", "")
 	u, err := c.URL()
 	require.NoError(t, err)
-	require.Equal(t, "postgres://postgres:pass@0.0.0.0:5432/?sslmode=disable", u.String())
+	require.Equal(t, "postgres://postgres:pass@localhost:5432/?sslmode=disable", u.String())
 
 	// With DOCKER_HOST
 	t.Setenv("DOCKER_HOST", "tcp://host.docker.internal:2375")
